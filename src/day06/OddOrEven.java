d2f29deacd52098bd3c52be51bf3354308de1995
studio2recording <github@cybertekschool.com>
2019-11-16 15:54:03 -0500

    Remainder practice for odd or even Operator precedence , using () for addition

---
+++ src/day06/OddOrEven.java
@@ -0,0 +1,23 @@
+package day06;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+
+        int myNumber = 10;
+        int yourNumber = 11;
+
+        System.out.println("myNumber Remainder of dividing by 2 : " + myNumber % 2);
+
+        System.out.println("yourNumber Remainder of dividing by 2 : " + yourNumber % 2);
+
+        System.out.println("adding my number to your number : " + yourNumber + myNumber);
+
+        System.out.println("adding my number to your number : " + (yourNumber + myNumber));
+
+
+    }
+
+
+}
