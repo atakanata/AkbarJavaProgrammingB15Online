20c177b0ba0128cc659d9ba70cd8d8decfe35a33
studio2recording <github@cybertekschool.com>
2019-11-16 16:25:05 -0500

    Assigning variable value using other variable  int x = 10 ; int y = x for example , FINSISHED

--- src/day06/UsingTheValueOfOtherVariables.java
+++ src/day06/UsingTheValueOfOtherVariables.java
@@ -16,12 +16,16 @@ public class UsingTheValueOfOtherVariables {
         System.out.println("My Favorite Number " + myFavoriteNumber);
         System.out.println("Your Favorite Number " + yourFavoriteNumber);
 
-        // Create a variable called yourOrder , type String and assign a value
-
+        // Create a variable called yourOrder , type String
+        // and assign a value
         // Create another variable called myOrder and assign the value
         // to same value as yourOrder by copying
         // and just print them out.
+        String yourOrder = "Gucci Bag";
+        String myOrder = yourOrder;
 
+        System.out.println("Your order is " + yourOrder + "\n" +
+                "My order is " + myOrder);
 
     }
 
