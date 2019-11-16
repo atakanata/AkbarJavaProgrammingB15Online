885660ae3bd9de8589f09f38bfebbd6bfd218585
studio2recording <github@cybertekschool.com>
2019-11-16 11:30:32 -0500

    DAY 6 Warm up tasks : Sale calculator start

---
+++ src/day06/SaleCalculator.java
@@ -0,0 +1,30 @@
+package day06;
+
+public class SaleCalculator {
+
+    public static void main(String[] args) {
+
+        /*
+        * Day 6
+
+        WAKE UP TASK :
+
+        Create a program to calculate the final discounted price of an Item
+
+        Create 3 double variables  called  regularPrice , salePrice and discount
+
+        Ask user question using scanner for regularPrice and discountPercentage
+        And save the result into the variable
+
+        Calculate sale price using above information
+        For example :  80 regular price , 0.2 for discount  , salePrice —>> 64
+
+        Print out  : regular price is 80$ , discount is 0.2  and your got deal for 64 $
+        *
+        * */
+
+
+    }
+
+
+}
