f34c54bd893c28baea702fe8607dde942f11395f
studio2recording <github@cybertekschool.com>
2019-11-16 11:48:31 -0500

    DAY 6 Warm up tasks : Sale calculator end

--- src/day06/SaleCalculator.java
+++ src/day06/SaleCalculator.java
@@ -1,5 +1,7 @@
 package day06;
 
+import java.util.Scanner;
+
 public class SaleCalculator {
 
     public static void main(String[] args) {
@@ -22,6 +24,28 @@ public class SaleCalculator {
         Print out  : regular price is 80$ , discount is 0.2  and your got deal for 64 $
         *
         * */
+        Scanner blabla = new Scanner(System.in);
+
+        double regularPrice;
+        double salePrice;
+        double discount;
+
+        System.out.println("What is the regular price ");
+        regularPrice = blabla.nextDouble();
+
+        System.out.println("What is discount rate ");
+        discount = blabla.nextDouble();
+
+        salePrice = regularPrice - regularPrice * discount;
+
+        System.out.println("regular price is " + regularPrice
+                + "$ , discount is " + discount + "  and your got deal for " + salePrice + "    $");
+
+
+
+
+
+
 
 
     }
