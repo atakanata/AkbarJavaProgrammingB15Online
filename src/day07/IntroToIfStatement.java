d70b08acf49ed54729fff60b21bb53a52ce97aef
studio2recording <github@cybertekschool.com>
2019-11-17 17:57:52 -0500

    Intro to If statement

---
+++ src/day07/IntroToIfStatement.java
@@ -0,0 +1,31 @@
+package day07;
+
+public class IntroToIfStatement {
+
+
+    public static void main(String[] args) {
+
+
+        int score = 59;
+
+        //if( PUT YOUR CONDITION HERE ) {
+        //
+        //do this if the condition is true
+
+        // }
+
+        System.out.println(score > 60);
+
+        if (score > 60) {
+
+            System.out.println(" CHOCOLATE FOR YOU !!");
+
+        }
+
+        System.out.println("THE END ");
+
+
+    }
+
+
+}
