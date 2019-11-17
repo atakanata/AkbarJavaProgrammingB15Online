06676e753e9940b395f10e0f245c7b92cb53038b
studio2recording <github@cybertekschool.com>
2019-11-17 14:59:56 -0500

    Email builder String concatenation practice With Scanner

--- src/day07/EmailBuilder.java
+++ src/day07/EmailBuilder.java
@@ -1,18 +1,25 @@
 package day07;
 
+import java.util.Scanner;
+
 public class EmailBuilder {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your first name , last name , company seperated by space : ");
+
         String firstName, lastName, company, email;
-        firstName = "Astrit";
-        lastName = "Enver";
-        company = "Verizon";
+        firstName = scan.next();   //"Astrit";
+        lastName = scan.next();   //"Enver";
+        company = scan.next();   //"Verizon";
         // firstName_lastName@company.com    Astrit_Enver@Verizon.com
         email = firstName + "_" + lastName + "@" + company + ".com";
 
-        System.out.println(" My name is " + firstName + " " + lastName + " and I work for " + company + " "
-                + "and my email is " + email);
+        System.out.println(" My name is " + firstName + " "
+                + lastName + " and I work for " + company
+                + " and my email is " + email);
 
     }
 
