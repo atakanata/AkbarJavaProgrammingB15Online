09013cc36167018b6f11df904a8853d38431332b
studio2recording <github@cybertekschool.com>
2019-11-17 16:09:17 -0500

    casting primitive types to convert one data type to another

---
+++ src/day07/Casting.java
@@ -0,0 +1,28 @@
+package day07;
+
+public class Casting {
+
+    public static void main(String[] args) {
+
+        //int num = 10 ;
+
+        //int 10 is implicitly(automatically) converted to double 10.0
+        // and stored inside bigNum behind scene
+        double bigNum = 10;
+        System.out.println(bigNum);
+
+        // 12.99 is double , double has much bigger range than int
+        // so it just does not fit
+        //int num = 12.99 ;  // compiler error!!!!
+
+        int num = (int) 12.99;
+        System.out.println(num);
+
+        // create a long variable and store int
+        // create an int variable and try to store above long value
+
+
+    }
+
+
+}
