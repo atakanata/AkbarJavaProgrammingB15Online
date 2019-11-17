a0d0b5b7e8d25b9e3b74245589344f20789fdd85
studio2recording <github@cybertekschool.com>
2019-11-17 16:58:14 -0500

    coverting between number and characters

---
+++ src/day07/CastingTheCharacter.java
@@ -0,0 +1,31 @@
+package day07;
+
+public class CastingTheCharacter {
+
+    public static void main(String[] args) {
+
+        char grade = 'B';
+        System.out.println(grade);
+
+        //'B' is represented by 66 in ascii table
+        // here type char is automatically widened to int
+        // and stored as number
+        int letterInNumber = 'B';
+        System.out.println(letterInNumber);
+
+        int letterInNumber2 = 'b';
+        System.out.println(letterInNumber2);
+
+        // -------------------------------
+        // saving the number as char
+        // so we can get the character representation
+        // of the number in ascii table
+        char myFirstChar = (char) 100;
+        System.out.println(myFirstChar);
+
+        // find out your number of each character in your first name
+
+    }
+
+
+}
