302521a515799c61f8d27a6e65922fd634af55f9
studio2recording <github@cybertekschool.com>
2019-11-17 17:58:12 -0500

    Intro to if else statement

---
+++ src/day07/IntroToIfElseStatement.java
@@ -0,0 +1,27 @@
+package day07;
+
+public class IntroToIfElseStatement {
+
+
+    public static void main(String[] args) {
+
+        int score = 79;
+        //if( PUT YOUR CONDITION HERE ) {
+        //     do this if the condition is true
+        // } else {
+        //      do this if it is not true
+        // }
+
+        if (score > 60) {
+            System.out.println(" CHOCOLATE FOR YOU !!");
+        } else {
+            System.out.println("  Try harder next time !! not tv , no phone , DO NOT GIVE UP ");
+        }
+
+        System.out.println("THE END ");
+
+
+    }
+
+
+}
