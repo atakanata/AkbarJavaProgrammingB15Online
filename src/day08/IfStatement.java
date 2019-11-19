8cf90f3254f14b4e3a37518be2009a27ff99c07d
studio2recording <github@cybertekschool.com>
2019-11-18 20:48:31 -0500

    Day 07 : If Statement for with more actions

---
+++ src/day08/IfStatement.java
@@ -0,0 +1,48 @@
+package day08;
+
+public class IfStatement {
+
+    public static void main(String[] args) {
+
+
+        int speedLimit = 60;
+        int yourCurrentSpeed = 145;
+        // if the yourCurrentSpeed is more than speed limit
+        // get pulled over by the police
+        // given ticket by the police
+        // taken away some points on your license
+        // go to court
+
+        boolean IamSpeading = yourCurrentSpeed > speedLimit;
+        // INSIDE PERENTHESIS WE CAN ONLY PUT
+        // BOOLEAN VALUE  : ture false
+        // BOOLEAN VARIABLE
+        // ANY EXPRESSION THAT RETURN BOOLEAN VALUE
+
+        //if( IamSpeading ) {
+        // if( true ) {
+        if (yourCurrentSpeed > speedLimit) {
+
+            System.out.println("get pulled over by the police");
+            System.out.println("given ticket by the police");
+            System.out.println("taken away some points on your license");
+            System.out.println("go to court");
+        } else {
+            System.out.println("go shopping !!");
+            System.out.println("Buy IceCream !!");
+            System.out.println("Enjoy your Day !!");
+        }
+
+        System.out.println("THE END");
+
+        // if not over the limit
+        // go shopping !!
+
+
+        // outside if statement , say the end
+
+
+    }
+
+
+}
