e207e6c9a425f85264e06c8f5df181188edbc1c5
studio2recording <github@cybertekschool.com>
2019-11-23 17:08:13 -0500

    DAY 11 : String equality check

---
+++ src/day11/StringEquality.java
@@ -0,0 +1,31 @@
+package day11;
+
+public class StringEquality {
+
+    public static void main(String[] args) {
+
+        String name = "Rabia";  // String literal
+        String name2 = new String("Rabia");
+        String name3 = "Rabia";
+
+        System.out.println(name);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println("USING == METHODS");
+        System.out.println(name == name2);
+        System.out.println(name == name3);
+
+        //  The correct way of comparing String to get consistent result is
+        // using one of the String object behaviour / method called
+        // equals method
+        //   str1.equals(str2)
+        System.out.println("USING EQUALS METHODS");
+        System.out.println(name.equals(name2));
+        System.out.println(name.equals(name3));
+
+        // BOTTOM LINE IS : DO NOT EVER USE == TO CHECK STRING EQUALITY
+
+
+    }
+
+}
