8cf123d500d0b1621dfba37b973eafc01d280332
studio2recording <github@cybertekschool.com>
2019-11-24 11:42:44 -0500

    DAY 12 : Warm up task 1 on weather using if else if else and equals method

---
+++ src/day12/WakeUp_CodeInWeather.java
@@ -0,0 +1,26 @@
+package day12;
+
+public class WakeUp_CodeInWeather {
+
+    public static void main(String[] args) {
+
+        String weather = "Rainy";
+
+        if (weather.equals("Sunny")) {
+            System.out.println("Code in Sunny weather ");
+        } else if (weather.equals("Rainy")) {
+            System.out.println("Code in Rainy weather ");
+        } else if (weather.equals("Cloudy")) {
+            System.out.println("Code in Cloudy weather ");
+        } else if (weather.equals("Snowy")) {
+            System.out.println("Code in Snowy weather ");
+        } else {
+
+            System.out.println("rain or shine just keep coding anyway !!! ");
+
+        }
+
+
+    }
+
+}
