238d4b474390f16528dc3ecfb066a8c66fff34a9
studio2recording <github@cybertekschool.com>
2019-11-24 14:06:52 -0500

    DAY 12 : Login Test : String Equality + Logical Operator

---
+++ src/day12/LoginTest.java
@@ -0,0 +1,51 @@
+package day12;
+
+public class LoginTest {
+
+
+    public static void main(String[] args) {
+
+        // we can declare and assign value like this
+        // if it's multiple variables of same type
+        String userName = "AAA", password = "pass123";
+
+        // user123 , pass123
+
+        //System.out.println( ! userName.equals("user123")   );
+
+        if (userName.equals("user123") && password.equals("pass123")) {
+
+            System.out.println("Login Successful ");
+
+        } else if (!userName.equals("user123") && password.equals("pass123")) {
+            System.out.println("USERNAME NOT CORRECT!!!");
+
+        } else if (userName.equals("user123") && !password.equals("pass123")) {
+
+            System.out.println("PASSWORD NOT CORRECT");
+
+        } else {
+            System.out.println(" USERNAME AND PASSWORD ARE BOTH WRONG !!!!");
+        }
+
+
+    }
+
+}
+// TASK 4
+
+//    String str = “abc” ;
+//        str.equals(“abc”) —> true       ! str.equals(“abc”) —>> false
+//
+//        Create class called LoginTest with main method
+//        Create 2 String variable called userName , password
+//        and do following
+//        // your correct username password is  user123  and pass123
+//        Check if your username and password correct
+//        If so print login successful
+//
+//        If not
+//        Check whether your username correct
+//        if not print  , username is not correct
+//        Check your password is correct or not
+//        If not print password is not correct
