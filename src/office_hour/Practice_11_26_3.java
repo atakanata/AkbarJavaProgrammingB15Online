dd37c1edf446220c529c0dca228476fa3683f1b3
CybertekStudios1 <github@cybertekschool.com>
2019-11-26 18:58:23 -0500

    Day 14 : OFFICE HOUR Switch statement to have same action for multiple branches

---
+++ src/office_hour/Practice_11_26_3.java
@@ -0,0 +1,55 @@
+package office_hour;
+
+public class Practice_11_26_3 {
+
+    public static void main(String[] args) {
+
+        // if the season is
+        // spring , summer , fall --> print bear is active
+        // winter -->> print bear sleep :
+
+        String season = "spring";
+
+        switch (season) {
+
+            case "spring":
+//                System.out.println("bear is active ");
+//                break;
+            case "summer":
+//                System.out.println("bear is active ");
+//                break;
+            case "fall":
+                System.out.println("bear is active ");
+                break;
+            case "winter":
+                System.out.println("bear is sleeping ");
+                break;
+
+        }
+
+
+//        if(season.equals("spring")  || season.equals("summer") || season.equals("fall")  ){
+//
+//            System.out.println("bear is active ");
+//
+//        }else if(season.equals("winter")){
+//
+//            System.out.println("bear is sleeping ");
+//
+//        }
+
+
+//        if(season.equals("spring")   ){
+//            System.out.println("bear is active ");
+//        }else if(season.equals("summer")  ){
+//            System.out.println("bear is active ");
+//        }else if(season.equals("fall")  ){
+//            System.out.println("bear is active ");
+//        }else if(season.equals("winter")  ){
+//            System.out.println("bear is sleeping ");
+//        }
+
+
+    }
+
+}
