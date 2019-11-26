9cddfb9390a61af1e216c665a2304fc98fb877fd
studio2recording <github@cybertekschool.com>
2019-11-25 22:11:59 -0500

    DAY 13 : String method starWith and endWith

---
+++ src/day13/TestStringStartWithOrEndWith.java
@@ -0,0 +1,27 @@
+package day13;
+
+public class TestStringStartWithOrEndWith {
+
+    public static void main(String[] args) {
+
+        String name = "Muge";
+
+
+        // check if a String start with another string
+        // IT IS CASE SENSITIVE
+        System.out.println(name.startsWith("Mu"));
+        System.out.println(name.startsWith("mu"));
+        System.out.println(name.startsWith("Mac"));
+
+
+        // check if a String end with another string
+
+        System.out.println(name.endsWith("e"));
+        System.out.println(name.endsWith("A"));
+        System.out.println(name.endsWith("E"));
+
+
+    }
+
+
+}
