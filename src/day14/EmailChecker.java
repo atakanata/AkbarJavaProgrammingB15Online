8eca2735215e6f84b068bb1af11ff54868fbdec5
CybertekStudios1 <github@cybertekschool.com>
2019-11-26 20:53:04 -0500

    Day 14 : Email Checker

---
+++ src/day14/EmailChecker.java
@@ -0,0 +1,41 @@
+package day14;
+
+public class EmailChecker {
+
+    public static void main(String[] args) {
+
+//        Create a class called EmailChecker with main method:
+//
+//        create a variable called email
+//
+//        check for below conditions
+//
+//        if it does not contain character @  say invalid email
+//        if it contain space say invalid email
+//        if it endsWith @gmail.com  -->> print gmail
+//        if it endsWith @yahoo.com  -->> print yahoo email
+//        if it endsWith @mail.ru  -->> print russian email
+        String email = "abc@gmail.com";
+
+        if (!email.contains("@") || email.contains(" ")) {
+
+            System.out.println("invalid email");
+
+        } else if (email.endsWith("@gmail.com")) {
+
+            System.out.println("Google Mail ");
+
+        } else if (email.endsWith("@yahoo.com")) {
+
+            System.out.println("Yahoo Mail");
+
+        } else if (email.endsWith("@mail.ru")) {
+
+            System.out.println("Russian mail");
+
+        }
+
+
+    }
+
+}
