f86bb97193475dc4c399bdd752cea40225c67635
CybertekStudios1 <github@cybertekschool.com>
2019-11-26 20:08:24 -0500

    Day 14 : String Review length() contains  startWith endWith

--- src/day14/StringReview.java
+++ src/day14/StringReview.java
@@ -33,38 +33,6 @@ public class StringReview {
         boolean endWithPumpkin = str.endsWith("Pumpkin");
         System.out.println("end With Pumpkin = " + endWithPumpkin);
 
-        //  PASSWORD VALIDATOR
-
-        // MINIMUM 8 CHAR MAX 16 CHAR
-        // IT MUST CONTAINS _ OR $
-        // IT MUST NOT CONTAINS SPACE
-        // IT MUST START Ab
-
-        // if any of above condition does not match say INVALID PASSWORD
-        // else say  GOOD PASSWORD !
-        String password = "ASH2723gfgdsu";
-        // MINIMUM 8 CHAR MAX 16 CHAR
-        boolean min8max16 = password.length() >= 8 && password.length() <= 16;
-
-        // IT MUST CONTAINS _ OR $
-        boolean mustContains_or$ = password.contains("_") || password.contains("$");
-        //
-
-        // IT MUST NOT CONTAINS SPACE
-        boolean mustNotContainsSpace = !password.contains(" ");
-        //
-
-        // IT MUST START Ab
-        boolean mustStartsWithAb = password.startsWith("Ab");
-        //
-
-        if (min8max16 && mustContains_or$ && mustNotContainsSpace && mustStartsWithAb) {
-            System.out.println("VALID PASSWORD");
-        } else {
-            System.out.println("INVALID PASSOWRD");
-        }
-
-
     }
 
 }
