f9667e7f97fea27ff665851e3743ebbde989755a
CybertekStudios1 <github@cybertekschool.com>
2019-11-27 21:38:42 -0500

    Day 15 : String substring method practice

--- src/day15/SubStringPractice.java
+++ src/day15/SubStringPractice.java
@@ -19,13 +19,45 @@ public class SubStringPractice {
         System.out.println("wordThe = " + wordThe);
 
         // Get from the second word till last word
-        //             0123456789012345
+        //                 0123456789012345
         //String movie =  "Lord Of The Ring" ;
-        int startingPoint = movie.indexOf(" ") + 1;
-        int endingPoint = movie.length();
 
-        System.out.println("second word till last :"
-                + movie.substring(startingPoint, endingPoint));
+
+        String wordLordOf = movie.substring(0, 7);
+        System.out.println("wordLordOf = " + wordLordOf);
+
+//        int startingPoint = movie.indexOf(" ") + 1;
+//        int endingPoint = movie.length();
+        // assume we already know the location of starting point of second word
+        // and the ending of the sentence
+
+        String secondWordTillLast = movie.substring(5, 16);
+        System.out.println("second Word TillLast = " + secondWordTillLast);
+
+        // If you want to start from somewhere and just get the rest of the String
+        // above method will work , however there is even better method
+        // if you provide only one parameter to substring method
+        // it will just start from that location and get till the end
+        String secondToLast = movie.substring(5);
+
+
+        // with this in mind : TASK
+        // YOU KNOW THE INDEX ALREADY
+        //                 0123456789012345
+        //String movie =  "Lord Of The Ring" ;
+        // TURN-->> Lord Of The Ring -->> Ring Of The Lord
+        String wordRing = movie.substring(12);
+        String wordOfThe = movie.substring(4, 12);
+        String wordLord = movie.substring(0, 4);
+
+        System.out.println(wordRing + wordOfThe + wordLord);
+
+        // TURN-->> Lord Of The Ring -->> Ring Of The Java Lord
+        System.out.println(wordRing + wordOfThe + "Java " + wordLord);
+
+
+//        System.out.println("second word till last :"
+//                + movie.substring(startingPoint, endingPoint));
 
 
     }
