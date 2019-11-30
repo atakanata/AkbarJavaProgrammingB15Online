771a34d975980c148158aef12a2e48b36c4223e9
CybertekStudios1 <github@cybertekschool.com>
2019-11-30 13:02:45 -0500

    Day 16 : String Practice Getting words of the sentence

--- src/day16/SecondWordGetter.java
+++ src/day16/SecondWordGetter.java
@@ -18,6 +18,8 @@ public class SecondWordGetter {
 
         //                 0123456789012345678
         String sentence = "You Understand Java";
+        System.out.println("sentence = " + sentence);
+
         String secondWord;
         int firstSpaceIndex = sentence.indexOf(" ");
         System.out.println("firstSpaceIndex = " + firstSpaceIndex);
@@ -25,13 +27,20 @@ public class SecondWordGetter {
         int lastSpaceIndex = sentence.lastIndexOf(" ");
         System.out.println("lastSpaceIndex = " + lastSpaceIndex);
 
-        System.out.println(sentence.substring(1 + 1, 6));
+        //System.out.println(sentence.substring(1 + 1, 6));
         secondWord = sentence.substring(firstSpaceIndex + 1, lastSpaceIndex);
         System.out.println("secondWord = " + secondWord);
 
         //// How to get first word :
         // in plain english : first word is stating from first character till first space
+        String firstWord = sentence.substring(0, firstSpaceIndex);
+        // how do we print out a variable with value using shortcut : soutv then tab
+        System.out.println("firstWord = " + firstWord);
 
+        // how to get last word from any sentence
+        // last word in a sentence means  from last space till the rest
+        String lastWord = sentence.substring(lastSpaceIndex);
+        System.out.println("lastWord = " + lastWord);
 
     }
 
