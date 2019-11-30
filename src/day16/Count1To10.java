785994d10ce3e6c54763e1b6e0d60f7eb09972de
CybertekStudios1 <github@cybertekschool.com>
2019-11-30 17:15:54 -0500

    Day 16 : Loop : While loop Count to 10

---
+++ src/day16/Count1To10.java
@@ -0,0 +1,22 @@
+package day16;
+
+public class Count1To10 {
+
+    public static void main(String[] args) {
+
+        int counter = 1;
+
+        while (counter < 11) {
+
+            System.out.println("Counter values : " + counter);
+            // SOMETHING HAS TO BE DONE TO CHANGE THE CONDITION ALONG THE WAY
+            // OR IT JUST KEEP RUNNING FOREVER
+            ++counter;
+
+        }
+
+
+    }
+
+
+}
