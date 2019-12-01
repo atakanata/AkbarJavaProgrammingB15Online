af4ddd229d0d3106deffca22e92b1e27b2cc8699
CybertekStudios1 <github@cybertekschool.com>
2019-12-01 15:50:08 -0500

    Day 17 : While Loop | Odd and Even !

---
+++ src/day17/OnlyEvenNumber.java
@@ -0,0 +1,22 @@
+package day17;
+
+public class OnlyEvenNumber {
+
+    public static void main(String[] args) {
+
+        int x = 100;
+
+
+        while (x >= 0) {
+
+            if (x % 2 == 0) {
+                System.out.println(x + " is even number");
+            }
+            --x;
+
+        }
+
+
+    }
+
+}
