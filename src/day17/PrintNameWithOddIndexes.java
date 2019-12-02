835dd5b20603a01179ab473c9e0200d63c751781
CybertekStudios1 <github@cybertekschool.com>
2019-12-02 18:11:52 -0500

    Day 17 : String Review and While Loop Practice

---
+++ src/day17/PrintNameWithOddIndexes.java
@@ -0,0 +1,34 @@
+package day17;
+
+import java.util.Scanner;
+
+public class PrintNameWithOddIndexes {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("ENTER YOUR NAME !!");
+        String name = scan.nextLine();
+
+        int x = 0;
+        // x <= name .length()-1  meaning :
+        // starting from 0 until last index
+
+        // x < name .length()  meaning :
+        // starting from 0 till right before the count of characters 
+
+
+        while (x < name.length()) {  // x <= name .length()-1 )
+
+            if (x % 2 == 1) {
+                System.out.println(name.charAt(x));
+            }
+            ++x;
+        }
+
+
+    }
+
+
+}
