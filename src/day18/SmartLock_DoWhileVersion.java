b0a8c808c00a3c5d78731ee0eb37e3de62d95771
CybertekStudios1 <github@cybertekschool.com>
2019-12-02 20:46:28 -0500

    Day 18 : Smart Lock Do while with !!!

---
+++ src/day18/SmartLock_DoWhileVersion.java
@@ -0,0 +1,33 @@
+package day18;
+
+import java.util.Scanner;
+
+public class SmartLock_DoWhileVersion {
+
+
+    public static void main(String[] args) {
+
+        // smart lock door has password
+        // unless you enter correct password
+        // it keep locking you out
+        // assuming correct password is B15
+        // keep asking until user enter correct one
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("KNOCK KNOCK !!");
+
+        String password;
+
+        do {
+            System.out.println("WHAT'S THE PASSWORD ? ");
+            password = scan.next();
+
+        } while (!password.equals("B15"));
+
+        System.out.println("DOOR IS OPEN!!!!");
+
+
+    }
+
+}
