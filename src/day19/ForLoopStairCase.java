f29503e8faba643974b86d33cac2820b0ce2601a
CybertekStudios1 <github@cybertekschool.com>
2019-12-03 20:29:13 -0500

    Day 19 : For loop Star Stair case

---
+++ src/day19/ForLoopStairCase.java
@@ -0,0 +1,37 @@
+package day19;
+
+public class ForLoopStairCase {
+
+    //    *
+//    * *
+//    * * *
+//    * * * *
+    public static void main(String[] args) {
+
+
+//        star = star + "* " ;
+//        System.out.println(star);
+//
+//        star = star + "* " ;
+//        System.out.println(star);
+//
+//        star = star + "* " ;
+//        System.out.println(star);
+//
+//        star = star + "* " ;
+//        System.out.println(star);
+        String star = "";
+
+        for (int i = 1; i <= 100; i++) {
+
+            //star = star + "* " ;
+            star += "* ";
+            System.out.println(star);
+
+        }
+
+
+    }
+
+
+}
