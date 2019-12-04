45dcae7cff2c23c1c63c49e7f28b1aaefffa7c3b
CybertekStudios1 <github@cybertekschool.com>
2019-12-03 20:02:08 -0500

    Day 19 : Speed up without scanner

---
+++ src/day19/WarmUp_SpeedUp.java
@@ -0,0 +1,29 @@
+package day19;
+
+public class WarmUp_SpeedUp {
+
+    public static void main(String[] args) {
+
+        // this is how we counted from 1 to 10
+
+//        for(int i = 1 ; i <= 10 ; i++){
+//
+//            System.out.print(i + " " );
+//
+//        }
+
+        int start = 5;
+        int end = 19;
+
+        System.out.print("you have started at speed-->>  ");
+        for (int i = start; i <= end; i++) {
+
+            System.out.print(i + " ");
+
+        }
+
+
+    }
+
+
+}
