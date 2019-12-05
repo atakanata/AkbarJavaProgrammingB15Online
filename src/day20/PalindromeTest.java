b7fee6458346f39060f00f8b2b7b087e2a76f0ca
CybertekStudios1 <github@cybertekschool.com>
2019-12-04 20:51:52 -0500

    Day 20 : For loop review : Palindrome Test

---
+++ src/day20/PalindromeTest.java
@@ -0,0 +1,36 @@
+package day20;
+
+public class PalindromeTest {
+
+
+    // if you reverse a String if it does not change
+    // it means the string is Palindrome
+    public static void main(String[] args) {
+
+        // level , kayak , elle , madam , aziza
+
+        String name = "Nurses run"; //nur sesruN
+        System.out.println("name = " + name);
+
+        String reversedName = "";
+
+        for (int x = name.length() - 1; x >= 0; x--) {
+            reversedName = reversedName + name.charAt(x);
+        }
+
+        System.out.println("reversedName = " + reversedName);
+
+        // Check whether reversed name is equal without case checking
+        if (name.equalsIgnoreCase(reversedName)) {
+
+            System.out.println("PALINDROME TEST HAS PASSED");
+
+        } else {
+            System.out.println("PALINDROME TEST HAS FAILED");
+        }
+
+
+    }
+
+
+}
