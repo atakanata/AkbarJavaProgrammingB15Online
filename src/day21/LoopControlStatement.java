7518c839c608795d2226f3f136b95f18876611a6
CybertekStudios1 <github@cybertekschool.com>
2019-12-07 14:06:55 -0500

    Day 21 : break , continue intro , YOU FIX IT

---
+++ src/day21/LoopControlStatement.java
@@ -0,0 +1,57 @@
+package day21;
+
+public class LoopControlStatement {
+
+    public static void main(String[] args) {
+
+        // break
+        // if line contains break reaches, the loop will stop !!!
+
+
+//        for (int i = 1; i <= 10  ; i++) {
+//
+//            System.out.println(i);
+//            break;
+//
+//        }
+
+        // while iterating over 1 to 10
+        // break out of the loop when it's 5
+
+//        for (int i = 1; i <= 10 ; i++) {
+//
+//            System.out.println(i);
+//            if(i==5){
+//                break ;
+//            }
+//
+//        }
+
+
+        int sum = 0;
+
+        for (int x = 1; x <= 10; x++) {
+
+            System.out.println("current sum is " + sum);
+
+
+            if (sum > 40) {
+                break;
+            }
+            // we need to add the sum after we make sure it did not go over 40
+            sum = sum + x;
+
+        }
+        System.out.println("sum = " + sum);
+
+
+        // continue
+        // if line contains break reaches,  the loop will skip to next iteration
+
+        // WE CAN USE break and continue for any type of loop
+
+
+    }
+
+
+}
