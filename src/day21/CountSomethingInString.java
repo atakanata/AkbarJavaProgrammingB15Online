c27ca8bcddce28691320258d96ee7ac407f54b64
CybertekStudios1 <github@cybertekschool.com>
2019-12-07 14:19:44 -0500

    Day 21 : Searching for 3 neighboring characters and counting in a sentence

--- src/day21/CountSomethingInString.java
+++ src/day21/CountSomethingInString.java
@@ -30,7 +30,7 @@ public class CountSomethingInString {
         int counter = 0;
 
         System.out.println("---counting certain 3 chars ---");
-        for (int x = 0; x < charCount - 3; x++) {  // will do the same in different way
+        for (int x = 0; x <= charCount - 3; x++) {  // will do the same in different way
             //for (int x = 0; x <= lastCharIndex - 2; x++) {
 
             // saving the current 3 characters
