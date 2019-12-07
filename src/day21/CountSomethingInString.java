445bbad30b8452a72f23844f46c216587ebffac9
CybertekStudios1 <github@cybertekschool.com>
2019-12-07 13:40:23 -0500

    Day 21 : Nice and Easy way to n character at a time from a String

--- src/day21/CountSomethingInString.java
+++ src/day21/CountSomethingInString.java
@@ -30,8 +30,8 @@ public class CountSomethingInString {
         int counter = 0;
 
         System.out.println("---counting certain 3 chars ---");
-        //for (int x = 0; x < charCount-3 ; x++) {  // will do the same in different way
-        for (int x = 0; x <= lastCharIndex - 2; x++) {
+        for (int x = 0; x < charCount - 3; x++) {  // will do the same in different way
+            //for (int x = 0; x <= lastCharIndex - 2; x++) {
 
             // saving the current 3 characters
             String current3Chars = myName.substring(x, x + 3);
---
+++ src/day21/DecidingWhenToStop.java
@@ -0,0 +1,26 @@
+package day21;
+
+public class DecidingWhenToStop {
+
+
+    public static void main(String[] args) {
+
+        String msg = "Hello World";
+
+        int charCount = msg.length();
+
+        int n = 7;
+
+        //  x <= charCount-n
+
+        for (int x = 0; x <= charCount - n; x++) {
+
+            System.out.println(msg.substring(x, x + n));
+
+        }
+
+
+    }
+
+
+}
