44acfbde41e876d659bb5cb1afe5538128d354ca
CybertekStudios1 <github@cybertekschool.com>
2019-12-07 16:47:46 -0500

    Day 21 : Loop Control Statement : Unreachable code

---
+++ src/day21/OrderOfBreakContinue.java
@@ -0,0 +1,24 @@
+package day21;
+
+public class OrderOfBreakContinue {
+
+
+    public static void main(String[] args) {
+
+
+        for (int x = 1; x <= 10; x++) {
+
+            //break ;
+            continue;
+            // THIS WILL NOT WORK , UNREACHABLE CODE !!!
+            //System.out.println("HELLO");
+
+        }
+
+        System.out.println("THE END ");
+
+
+    }
+
+
+}
