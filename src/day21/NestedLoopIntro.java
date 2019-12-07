924b6f75825da625625a5c5e07ba577f5a1945a8
CybertekStudios1 <github@cybertekschool.com>
2019-12-07 17:05:41 -0500

    Day 21 : Nested Loop

---
+++ src/day21/NestedLoopIntro.java
@@ -0,0 +1,45 @@
+package day21;
+
+public class NestedLoopIntro {
+
+
+    public static void main(String[] args) {
+
+
+        // count from 1 to 5
+
+        // repeat this 3 times
+
+//        for (int x = 1; x <= 5; x++) {
+//            System.out.print( x + " " );
+//        }
+//        System.out.println();
+//
+//        for (int x = 1; x <= 5; x++) {
+//            System.out.print( x + " " );
+//        }
+//        System.out.println();
+//
+//        for (int x = 1; x <= 5; x++) {
+//            System.out.print( x + " " );
+//        }
+//        System.out.println();
+
+
+        for (int i = 1; i <= 3; i++) {
+
+            System.out.println("ITERATION : " + i);
+
+            for (int x = 1; x <= 5; x++) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+
+
+        }
+
+
+    }
+
+
+}
