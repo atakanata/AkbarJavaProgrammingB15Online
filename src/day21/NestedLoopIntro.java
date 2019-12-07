7bc33c98b12245ac320bb5d8a9af26da672edb7c
CybertekStudios1 <github@cybertekschool.com>
2019-12-07 18:11:42 -0500

    Day 21 : Loop Practice ,break|continue  Nested Loop

--- src/day21/NestedLoopIntro.java
+++ src/day21/NestedLoopIntro.java
@@ -6,9 +6,7 @@ public class NestedLoopIntro {
     public static void main(String[] args) {
 
 
-        // count from 1 to 5
 
-        // repeat this 3 times
 
 //        for (int x = 1; x <= 5; x++) {
 //            System.out.print( x + " " );
@@ -24,7 +22,6 @@ public class NestedLoopIntro {
 //            System.out.print( x + " " );
 //        }
 //        System.out.println();
-
 
         for (int i = 1; i <= 3; i++) {
 
--- src/day21/NumberStairCase.java
+++ src/day21/NumberStairCase.java
@@ -14,7 +14,7 @@ public class NumberStairCase {
     public static void main(String[] args) {
 
 
-        for (int x = 1; x <= 100; x++) {
+        for (int x = 1; x <= 5; x++) {
             //System.out.println("x  :" + x );
 
 
