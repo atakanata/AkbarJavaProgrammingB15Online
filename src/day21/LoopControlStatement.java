052d66af6b28088f142e3ee55c69f4b9e054dcdc
CybertekStudios1 <github@cybertekschool.com>
2019-12-07 15:27:08 -0500

    Day 21 : Loop Control Statement : break

--- src/day21/LoopControlStatement.java
+++ src/day21/LoopControlStatement.java
@@ -27,20 +27,32 @@ public class LoopControlStatement {
 //
 //        }
 
+        // I want to keep adding money to my bank account from day 1 till day 10
+        // day 1 I add 1 $
+        // day 2 I add 2 $
+        // day 3 I add 3 $
+        //....
+        // day 10 I add 10 $
+
+        // however my bank account can only hold 12 $
+        // so I need to stop adding money the moment i realize it can not hold
+        // the money I am about put if the sum would go over 12
+
 
         int sum = 0;
 
         for (int x = 1; x <= 10; x++) {
 
             System.out.println("current sum is " + sum);
-
+            System.out.println("If I add " + x
+                    + " dollar , it would be " + (sum + x));
             // here we are checking whether after adding the next number
-            // it will go over 40 or not ,
+            // it will go over 12 or not ,
             // that's why we should use sum + x to see whether it will go over before we acrually add them
-            if (sum + x > 40) {
-                break;
+            if (sum + x > 12) {
+                break;   // BREAKING OUT OF THE LOOP
             }
-            // we need to add the sum after we make sure it did not go over 40
+            // we need to add the sum after we make sure it did not go over 12
             sum = sum + x;
 
         }
@@ -48,7 +60,7 @@ public class LoopControlStatement {
 
 
         // continue
-        // if line contains break reaches,  the loop will skip to next iteration
+        // if line contains continue reaches,  the loop will skip to next iteration
 
         // WE CAN USE break and continue for any type of loop
 
