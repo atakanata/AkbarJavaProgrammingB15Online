5437141ef7f8dfb3e810901cf51e6e817210272e
studio2recording <github@cybertekschool.com>
2019-12-10 22:13:01 -0500

    DAY 24 : Arrays Utility class method toString sort and equals methods

--- src/day24/ArraysEquality.java
+++ src/day24/ArraysEquality.java
@@ -11,26 +11,25 @@ public class ArraysEquality {
         int[] scores3 = {2, 5, 6, 7, 3, 34, 6};
         int[] scores4 = {6, 5, 6, 7, 3, 2, 34}; // it has same numbers as 3 but different order
 
-
         System.out.println(scores1 == scores2);
         System.out.println(scores1 == scores3);
         // for checking for equality of 2 array object content
         // equals  ---->> Arrays.equals(firstArray,secondArray)
         boolean isS1S2Equals = Arrays.equals(scores1, scores2);
-        System.out.println("is score1 has same content as score2  = " + isS1S2Equals);
+        System.out.println("does score1 has same content as score2  = " + isS1S2Equals);
 
         boolean isS1S3Equals = Arrays.equals(scores1, scores3);
-        System.out.println("is score1 has same content as score3  = " + isS1S3Equals);
+        System.out.println("does score1 has same content as score3  = " + isS1S3Equals);
 
         boolean isS3S4Equals = Arrays.equals(scores3, scores4);
-        System.out.println("is score3 has same content as score4  = " + isS3S4Equals);
+        System.out.println("does score3 has same content as score4  = " + isS3S4Equals);
 
         System.out.println("Sorting score 3 and score 4 then compare equality ");
         Arrays.sort(scores3);
         Arrays.sort(scores4);
 
         isS3S4Equals = Arrays.equals(scores3, scores4);
-        System.out.println("AFTER SORTING : is score3 has same content as score4  = " + isS3S4Equals);
+        System.out.println("AFTER SORTING : does score3 has same content as score4  = " + isS3S4Equals);
 
 
     }
