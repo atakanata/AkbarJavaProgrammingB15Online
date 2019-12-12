34dc54181b05e4a2adcdb170f5f61bf5ee8aac36
studio2recording <github@cybertekschool.com>
2019-12-11 20:49:38 -0500

    DAY 25 : String to char array with toCharArray then pring with for each loop

--- src/day25/StringToCharArray.java
+++ src/day25/StringToCharArray.java
@@ -8,6 +8,15 @@ public class StringToCharArray {
         // and use for each loop to loop over them
         // optionally count how many a you have in your name
 
+        String name = "Anastasiya";
+        // turn this into charArray using toCharArray() method of String
+
+        char[] allCharsInName = name.toCharArray();
+
+        for (char eachChar : allCharsInName) {
+            System.out.println("eachChar = " + eachChar);
+        }
+
 
     }
 
