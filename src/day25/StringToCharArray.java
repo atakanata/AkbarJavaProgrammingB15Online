9d4f6d8550f16932d09e7a975df25622b0374a59
studio2recording <github@cybertekschool.com>
2019-12-11 20:56:05 -0500

    DAY 25 : String to char array with toCharArray then count letter a with for each loop

--- src/day25/StringToCharArray.java
+++ src/day25/StringToCharArray.java
@@ -10,12 +10,23 @@ public class StringToCharArray {
 
         String name = "Anastasiya";
         // turn this into charArray using toCharArray() method of String
-
+        // toCharArray() is a method of String that turn string into char array
         char[] allCharsInName = name.toCharArray();
 
         for (char eachChar : allCharsInName) {
             System.out.println("eachChar = " + eachChar);
         }
+
+        // now count how many a we have
+        int count = 0;
+        for (char eachChar : allCharsInName) {
+
+            if (eachChar == 'a') {
+                ++count;
+            }
+
+        }
+        System.out.println("count = " + count);
 
 
     }
