e3e08cdea2398f3c24de73cfefa4525e049bd665
studio2recording <github@cybertekschool.com>
2019-12-11 21:15:42 -0500

    DAY 25 : String to char array with toCharArray then count letter a with for each loop, Sort char array

--- src/day25/StringToCharArray.java
+++ src/day25/StringToCharArray.java
@@ -1,5 +1,7 @@
 package day25;
 
+import java.util.Arrays;
+
 public class StringToCharArray {
 
     public static void main(String[] args) {
@@ -8,15 +10,22 @@ public class StringToCharArray {
         // and use for each loop to loop over them
         // optionally count how many a you have in your name
 
-        String name = "Anastasiya";
+
         // turn this into charArray using toCharArray() method of String
+
+        // WHAT DOES IT DO ?
         // toCharArray() is a method of String that turn string into char array
+        // DO I NEED TO PROVIDE EXTRA DATA WHILE CALLING THE METHOD ?
+        // NO
+        // WHAT DO I GET OUT OF IT ?
+        // char array object that has all the characters of the String object
+
+        String name = "Anastasiya";
         char[] allCharsInName = name.toCharArray();
-
-        for (char eachChar : allCharsInName) {
-            System.out.println("eachChar = " + eachChar);
-        }
-
+        System.out.println("BEFORE SORTING allCharsInName = " + Arrays.toString(allCharsInName));
+//        for (char eachChar : allCharsInName) {
+//            System.out.println("eachChar = " + eachChar);
+//        }
         // now count how many a we have
         int count = 0;
         for (char eachChar : allCharsInName) {
@@ -27,6 +36,11 @@ public class StringToCharArray {
 
         }
         System.out.println("count = " + count);
+
+        // What if you want to sort all characters of your name
+        // in alphabetical order ?
+        Arrays.sort(allCharsInName);
+        System.out.println("AFTER SORTING allCharsInName = " + Arrays.toString(allCharsInName));
 
 
     }
