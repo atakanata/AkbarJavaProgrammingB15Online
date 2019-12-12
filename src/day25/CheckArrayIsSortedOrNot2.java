e577e08621c4e6c77d7abd29f1140f9cda3659ad
studio2recording <github@cybertekschool.com>
2019-12-11 20:39:57 -0500

    DAY 25 : String to char array without toCharArray method

--- src/day25/CheckArrayIsSortedOrNot2.java
+++ src/day25/CheckArrayIsSortedOrNot2.java
@@ -8,12 +8,12 @@ public class CheckArrayIsSortedOrNot2 {
 
         //int[] nums = {13,31,8,5,21,2} ;
         int[] nums = {1, 2, 3, 4, 5, 6};
-        System.out.println("nums = " + Arrays.toString(nums));
+        System.out.println("num s = " + Arrays.toString(nums));
 
         // CREATE A VARIABLE CALLED isSortedAlready and assign value of true ;
         // I ASSUME THE ARRAY IS ALREADY SORTED
         // AND IF IT'S NOT THEN I CHANGE the value of isSortedAlready to FALSE
-        boolean isSortedAlready = true;
+        //boolean isSortedAlready = true;
 
         for (int x = 0; x < nums.length - 1; x++) {
             //System.out.println( nums[x] + " " + nums[x+1]);
@@ -25,11 +25,11 @@ public class CheckArrayIsSortedOrNot2 {
 
             if (!(nums[x] < nums[x + 1])) {
                 System.out.println("ARRAY IS NOT SORTED , NO POINT CHECKING THE REST");
-                isSortedAlready = false;
+                //isSortedAlready = false;
                 break;
             }
         }
-        System.out.println("isSortedAlready = " + isSortedAlready);
+        //System.out.println("isSortedAlready = " + isSortedAlready);
 
     }
 
---
+++ src/day25/StringToCharArray.java
@@ -0,0 +1,14 @@
+package day25;
+
+public class StringToCharArray {
+
+    public static void main(String[] args) {
+
+        // pick up your own name and turn in into char array
+        // and use for each loop to loop over them
+        // optionally count how many a you have in your name
+
+
+    }
+
+}
