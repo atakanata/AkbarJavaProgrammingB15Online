929e2027f35fcf258cceb90dd98d8bce5ffe14a5
studio2recording <github@cybertekschool.com>
2019-12-14 16:06:49 -0500

    Day 26 : Number more than 100 in array test logic 1.2

--- src/day26/ArrayTask_NumberTest_logic1.java
+++ src/day26/ArrayTask_NumberTest_logic1.java
@@ -1,6 +1,6 @@
 package day26;
 
-public class ArrayTask_NumberTest {
+public class ArrayTask_NumberTest_logic1 {
 
     /*
      * // 1, given an int array
@@ -9,6 +9,8 @@ public class ArrayTask_NumberTest {
     public static void main(String[] args) {
 
         int[] scores = {156, 101, 76, 187, 87, 110};
+        int size = scores.length;
+        System.out.println("size = " + size);
 
         // create a variable called finalResult to store
         // your result of checking if every number in this array is more than 100 or not
@@ -16,26 +18,27 @@ public class ArrayTask_NumberTest {
 // if all numbers are more than 100 then make it yes , if not make it no
 
         String finalResult = "";
-        // LOGIC 1 :
-        // take each item check if it is less than 100, count.
-        // in the end if count is more than 0, answer is NO,
+        // LOGIC 1.2 :
+        // take each item check if it is MORE than 100, increase the count.
+        // in the end if count is equal to array item count the finalResult Yes, answer is NO,
 
-        int cntLessThan100 = 0;
+        int cntMoreThan100 = 0;
         for (int eachNum : scores) {
 
-            if (eachNum <= 100) {
-                System.out.println("eachNum = " + eachNum);
-                cntLessThan100++;
+            if (eachNum > 100) {
+                ++cntMoreThan100; // increasing the counter
             }
 
         }
-        System.out.println("cntLessThan100 = " + cntLessThan100);
-        if (cntLessThan100 > 0) {
-            finalResult = "NO";
-        } else {
+        System.out.println("cntMoreThan100 = " + cntMoreThan100);
+        // if all the numbers are more than 100 , it will have same count as item count
+        if (cntMoreThan100 == size) {
             finalResult = "YES";
+        } else {
+            finalResult = "NO";
         }
         System.out.println("finalResult = " + finalResult);
+
 
         // LOGIC 2 :
         // Check each item and break when number is less than 100  after assigning final result to NO
