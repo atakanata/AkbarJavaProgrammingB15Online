f07af28188be47b0153df04933faaeb511e97d9c
studio2recording <github@cybertekschool.com>
2019-12-14 16:37:11 -0500

    Day 26 :Count item in an array

---
+++ src/day26/CountItemInAnArray.java
@@ -0,0 +1,26 @@
+package day26;
+
+import java.util.Arrays;
+
+public class CountItemInAnArray {
+
+    public static void main(String[] args) {
+
+
+        String[] marvelHeros = {"Iron Man", "Capitan America", "Spiderman",
+                "Balck Panther", "Hulk", "Black Widow", "Wanda", "Capitan Marvel",
+                "Capitan America", "Spiderman",
+                "Black Widow", "Wanda", "Capitan Marvel",
+                "Capitan America"};
+
+        System.out.println("Marvel Heros = " + Arrays.toString(marvelHeros));
+
+
+        String itemToSearch;
+        int countOfItem = 0;
+
+
+    }
+
+
+}
