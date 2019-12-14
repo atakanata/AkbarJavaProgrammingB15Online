cfbfc367504b20d7e1f7dd87af3a47898f49dbc5
studio2recording <github@cybertekschool.com>
2019-12-14 14:04:37 -0500

    Day 26 : Array Review : Swapping elements value to reverse an array

--- src/day26/ReverseAnArrayBySwapping.java
+++ src/day26/ReverseAnArrayBySwapping.java
@@ -11,15 +11,15 @@ public class ReverseAnArrayBySwapping {
         int lastIndex = size - 1;
         int middleIndex = size / 2; // 3
 
-        // just to see whats inside , we are not doing anything with String
-        System.out.println(Arrays.toString(myNumbers));
-
-        // first and last
-        System.out.println(myNumbers[0] + " --- " + myNumbers[lastIndex - 0]);
-        // second and one before last
-        System.out.println(myNumbers[1] + " --- " + myNumbers[lastIndex - 1]);
-        // third and two before last
-        System.out.println(myNumbers[2] + " --- " + myNumbers[lastIndex - 2]);
+//        // just to see whats inside , we are not doing anything with String
+//        System.out.println(Arrays.toString(myNumbers));
+//
+//        // first and last
+//        System.out.println(myNumbers[0] + " --- " + myNumbers[lastIndex - 0]);
+//        // second and one before last
+//        System.out.println(myNumbers[1] + " --- " + myNumbers[lastIndex - 1]);
+//        // third and two before last
+//        System.out.println(myNumbers[2] + " --- " + myNumbers[lastIndex - 2]);
 
         System.out.println("------ loop here ---------");
         // loop this
