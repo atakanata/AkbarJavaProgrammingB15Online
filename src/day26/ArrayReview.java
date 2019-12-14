6ad46614bc564e54f55250949749d1b2f0afa24d
studio2recording <github@cybertekschool.com>
2019-12-14 12:01:38 -0500

    Day 26 : Array Review : first hour

--- src/day26/ArrayReview.java
+++ src/day26/ArrayReview.java
@@ -25,13 +25,20 @@ public class ArrayReview {
 //
 //
 //        3 ways to create an array object and assign it to variable
-
+        // 1st way
         int[] myNumbers = new int[4];   // 4 is item count
         // we assign value using array variable and it's index
         myNumbers[0] = 10;
         myNumbers[1] = 40;
         myNumbers[2] = 30;
         myNumbers[3] = 7;
+
+        // 2nd way of creating an array
+        int[] myNumbers2 = new int[]{10, 40, 30, 7};
+        // 3rd way
+        // this declaring and assigning value should happen in ONE STATEMENT
+        int[] myNumbers3 = {10, 40, 30, 7};
+
 
         // how do we get the size of an array
         int size = myNumbers.length; // 4
@@ -59,8 +66,6 @@ public class ArrayReview {
         // just change my int array data type to int to avoid issues
 
         // 1.4 Swap the first item value with last item value
-
-
     }
 
 }
