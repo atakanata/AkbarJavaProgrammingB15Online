129a6b9220f97db9a044d128f61e4afafde9e34a
studio2recording <github@cybertekschool.com>
2019-12-14 16:49:17 -0500

    Day 26 :Count item in an String array with condition

--- src/day26/CountItemInAnArray.java
+++ src/day26/CountItemInAnArray.java
@@ -7,17 +7,32 @@ public class CountItemInAnArray {
     public static void main(String[] args) {
 
 
-        String[] marvelHeros = {"Iron Man", "Capitan America", "Spiderman",
-                "Balck Panther", "Hulk", "Black Widow", "Wanda", "Capitan Marvel",
-                "Capitan America", "Spiderman",
-                "Black Widow", "Wanda", "Capitan Marvel",
-                "Capitan America"};
+        String[] marvelHeroes = {"Iron Man", "Captain America", "Spider man",
+                "Black Panther", "Hulk", "Black Widow", "Wanda", "Captain Marvel",
+                "Captain America", "Spider man",
+                "Black Widow", "Wanda", "Captain Marvel",
+                "Captain America"};
 
-        System.out.println("Marvel Heros = " + Arrays.toString(marvelHeros));
+        System.out.println("Marvel Heros = " + Arrays.toString(marvelHeroes));
+        int sizeOfArray = marvelHeroes.length;
+        System.out.println("Hero count = " + sizeOfArray);
 
-
-        String itemToSearch;
+        String itemToSearch = "Black Widow";
         int countOfItem = 0;
+
+        for (String eachHero : marvelHeroes) {
+
+            if (eachHero.equals(itemToSearch)) {
+                countOfItem++;
+            }
+
+        }
+
+        // COUNT THE HERO WITH WITH THE NAME CONTAINS BLACK
+
+        System.out.println("countOfItem = " + countOfItem);
+
+
 
 
     }
