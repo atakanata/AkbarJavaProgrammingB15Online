15a6dc51928330f6b10fc0695abb9107fdb6511d
studio2recording <github@cybertekschool.com>
2019-12-16 19:29:47 -0500

    Day 28 : Multi D Array Review

---
+++ src/day28/ExcelVisualizer.java
@@ -0,0 +1,29 @@
+package day28;
+
+public class ExcelVisualizer {
+
+    public static void main(String[] args) {
+
+        /*
+      * Cell(1,1)= 78    Cell(1,2)= 54    Cell(1,3)= 100    Cell(1,4)= 84
+        Cell(2,1)= 33    Cell(2,2)= 44    Cell(2,3)= 77    Cell(2,4)= 123
+        Cell(3,1)= 12    Cell(3,2)= 88    Cell(3,3)= 52    Cell(3,4)= 76
+        Cell(4,1)= 67    Cell(4,2)= 33    Cell(4,3)= 98    Cell(4,4)= 67
+        Cell(5,1)= 12    Cell(5,2)= 88    Cell(5,3)= 52    Cell(5,4)= 45
+        Cell(6,1)= 67    Cell(6,2)= 33    Cell(6,3)= 98    Cell(6,4)= 34
+        *
+        * { {78,54,100,84},{33,44,77,123},{12,88,52,76},{67,33,98,67},{12,88,52,45},{67,33,98,34} };
+        * */
+        int[] row1 = new int[]{78, 54, 100, 84};
+        int[] row2 = new int[]{33, 44, 77, 123};
+        int[] row3 = new int[]{12, 88, 52, 76};
+        int[] row4 = new int[]{67, 33, 98, 67};
+        int[] row5 = new int[]{12, 88, 52, 45};
+        int[] row6 = new int[]{67, 33, 98, 34};
+
+        // to store this in another array object
+
+
+    }
+
+}
