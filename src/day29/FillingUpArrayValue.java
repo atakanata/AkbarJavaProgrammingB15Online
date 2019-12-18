4f17d33a7edace3c2c57fe906391fcd2c90472c9
studio2recording <github@cybertekschool.com>
2019-12-17 19:22:01 -0500

    Day 29 : Filling up array with 100 items

--- src/day29/FillingUpArrayValue.java
+++ src/day29/FillingUpArrayValue.java
@@ -7,9 +7,9 @@ public class FillingUpArrayValue {
     public static void main(String[] args) {
 
         // create an int array with size 100 and fill it up as below
-
+        // fill up this array with number from 1 to 100
         int[] numbers = new int[100];
-
+//                                           \n will move it to next line
         System.out.println("Before filling up \n" + Arrays.toString(numbers));
 //        numbers[0] = 1 ;
 //        numbers[1] = 2 ;
@@ -21,7 +21,7 @@ public class FillingUpArrayValue {
             numbers[x] = x + 1;
 
         }
-
+        //                                  \n will move it to next line
         System.out.println("After filling up \n" + Arrays.toString(numbers));
 
     }
