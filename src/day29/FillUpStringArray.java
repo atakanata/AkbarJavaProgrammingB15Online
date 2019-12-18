8b3ee807558fddcf0b4eba9cc0c4cf64accdb75e
studio2recording <github@cybertekschool.com>
2019-12-17 20:02:24 -0500

    Day 29 : Filling up String array with 300 I love Java

---
+++ src/day29/FillUpStringArray.java
@@ -0,0 +1,38 @@
+package day29;
+
+import java.util.Arrays;
+
+public class FillUpStringArray {
+
+    public static void main(String[] args) {
+
+        // Create an array with size 300 and fill it up with I Love Java
+        String[] strArr = new String[300];
+        System.out.println("before filling strArr = " + Arrays.toString(strArr));
+//        strArr[0] = "I Love Java";
+//        strArr[1] = "I Love Java";
+//        strArr[2] = "I Love Java";
+
+//        strArr[0] = "1.I Love Java";
+//        strArr[1] = "2.I Love Java";
+//        strArr[2] = "3.I Love Java";
+        // 300
+        for (int i = 0; i < strArr.length; i++) {
+            //strArr[i] =   "I Love Java";
+            strArr[i] = (i + 1) + ".I Love Java";
+        }
+        System.out.println("strArr = " + Arrays.toString(strArr));
+
+        String str = new String("ABC");   // THIS IS STRING OBEJCT WITH VALUE ABC
+        String[] strArr2 = new String[5]; // EMPTY STRING ARRAY WITH CAPACITY 5 ,
+        strArr2[0] = "i LOVE JAVA";
+        strArr2[1] = "i LOVE JAVA";
+        strArr2[2] = "i LOVE JAVA";
+        strArr2[3] = "i LOVE JAVA";
+        strArr2[4] = "i LOVE JAVA";
+        System.out.println("strArr2 = " + Arrays.toString(strArr2));
+
+
+    }
+
+}
