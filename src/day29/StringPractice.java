7ffc1a9be672ec25c8d8ae971a9062dbc41140ed
studio2recording <github@cybertekschool.com>
2019-12-17 20:46:33 -0500

    Day 29 : String Practice : split and initial practice

--- src/day29/StringPractice.java
+++ src/day29/StringPractice.java
@@ -29,6 +29,24 @@ public class StringPractice {
         // LOGIC 1  :  get the first letter of full name
         //          then get first letter after the space
         //          concatenate them
+        //          downside is what if we have middle name
+        //          solution , instead of looking for the letter after first space
+        //                      look for last index of the space then get next Character
+
+
+        // LOGIC 2  : We already have full name stored in fullName variable
+        //      split by space -->> we will get firstName (middle name) lastName
+        //      get the first letter of full name
+        //      then get the last name using last item index
+        //      get the first character then concatenate
+        //      Clark Kent -->> [  Clark , Kent ]   Clark->C   Kent-K  --->> CK
+        String[] fullNameSplitted = fullName.split(" ");
+        System.out.println("fullNameSplitted = " + Arrays.toString(fullNameSplitted));
+        // last name will be always the last element of splitted full name
+        // no matter the name has middle name or not
+        String lastName = fullNameSplitted[fullNameSplitted.length - 1];
+        System.out.println("lastName = " + lastName);
+        System.out.println("Initial of the hero is : " + fullName.charAt(0) + lastName.charAt(0));
 
 
 
