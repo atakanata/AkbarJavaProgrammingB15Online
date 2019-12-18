b9ab60a41797d7bf564ff7553463acef9811bf74
studio2recording <github@cybertekschool.com>
2019-12-17 20:32:00 -0500

    Day 29 : String Practice : split and initial

--- src/day29/StringPractice.java
+++ src/day29/StringPractice.java
@@ -16,6 +16,23 @@ public class StringPractice {
         String[] heroSplitted = hero1.split("-");
         System.out.println("heroSplitted = " + Arrays.toString(heroSplitted));
 
+        String heroCode = heroSplitted[0];
+        String fullName = heroSplitted[1];
+
+        // Hero code is Superman and Identity is Clark Kent
+        //System.out.println("Hero code is " + heroSplitted[0] + " and Identity is " + heroSplitted[1] );
+        System.out.println("Hero code is " + heroCode + " and Identity is " + fullName);
+
+        // The initial of the Hero is CK
+        // I can just use fullName variable to finish this task
+
+        // LOGIC 1  :  get the first letter of full name
+        //          then get first letter after the space
+        //          concatenate them
+
+
+
+
 
     }
 
