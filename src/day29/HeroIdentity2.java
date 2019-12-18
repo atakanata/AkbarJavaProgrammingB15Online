0d679971eeaa11449683882793c4e4daed19610d
studio2recording <github@cybertekschool.com>
2019-12-17 21:44:56 -0500

    Day 29 : String Practice : Hide Hero Identity using replace

--- src/day29/HeroIdentity2.java
+++ src/day29/HeroIdentity2.java
@@ -19,19 +19,21 @@ public class HeroIdentity2 {
         // generate stars with same length as full name character count
         // one the star is generated , replace full name with Stars
 
-//        String[] heroSplitted = hero1.split("-");
-//        String heroCode = heroSplitted[0];
-//        String fullName = heroSplitted[1];
+        String[] heroSplitted = hero1.split("-");
+        String heroCode = heroSplitted[0];
+        String fullName = heroSplitted[1];
 
         String stars = "";
-        for (int i = 1; i <= hero1.split("-")[1].length(); i++)
+        for (int i = 1; i <= fullName.length(); i++) {
             stars += "*";
+        }
+//        System.out.println("stars = " + stars);
 
-        //System.out.println("stars = " + stars);
-
-        //String heroX = hero1.replace(heroSplitted[1], stars);
+        String heroX = hero1.replace(fullName, stars);
         System.out.println("hero1 = " + hero1);
-        System.out.println("heroX = " + hero1.replace(hero1.split("-")[1], stars));
+        System.out.println("heroX = " + heroX);
+
+
 
 
     }
