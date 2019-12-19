4ea877d483c25b4567785c6c95fdf6f1edc0e885
studio2recording <github@cybertekschool.com>
2019-12-18 20:30:15 -0500

    Day 30 : Array Practice : Car Inventory print one type of car in a sentence

--- src/day30/CarInventory1000.java
+++ src/day30/CarInventory1000.java
@@ -1037,7 +1037,33 @@ public class CarInventory1000 {
 
         }
 
+        // Task 3 : PRINT Make and Model separately in this format
+        //Honda-Civic  -->>
+        // Car make is Honda and Model is Civic
+        // print only Honda brand cars
 
+
+//        String eachCar = "Honda-Civic" ;
+//        String[] eachCarSplitted =  eachCar.split("-");
+//
+//        System.out.println("Car make is " + eachCarSplitted[0] +
+//                            " Car Model is " + eachCarSplitted[1]   );
+
+        // we want to go through each and every car
+        // and we don't need index so we used for each loop
+        for (String eachCar : cars) {
+            // we want to do this action only for one type of car brand
+            // so we used start with method to check car brand
+            if (eachCar.startsWith("Honda")) {
+                // we used split method to separate brand and model
+                String[] eachCarSplitted = eachCar.split("-");
+                // first part is brand , second part is model
+                System.out.println("Car make is " + eachCarSplitted[0] +
+                        " Car Model is " + eachCarSplitted[1]);
+
+            }
+
+        }
 
 
     }
