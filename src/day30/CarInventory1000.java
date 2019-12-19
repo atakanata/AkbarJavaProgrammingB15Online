9074f1f751f893b994407d87c94ca02da6458dd7
studio2recording <github@cybertekschool.com>
2019-12-18 21:02:08 -0500

    Day 30 : Array Practice : Car Inventory print out only those cars with model has 2 character

--- src/day30/CarInventory1000.java
+++ src/day30/CarInventory1000.java
@@ -1058,12 +1058,36 @@ public class CarInventory1000 {
                 // we used split method to separate brand and model
                 String[] eachCarSplitted = eachCar.split("-");
                 // first part is brand , second part is model
-                System.out.println("Car make is " + eachCarSplitted[0] +
-                        " Car Model is " + eachCarSplitted[1]);
+//                System.out.println("Car make is " + eachCarSplitted[0] +
+//                        " Car Model is " + eachCarSplitted[1]);
 
             }
 
         }
+        System.out.println("------ car with model has only 2 characters---  ");
+        // find all the car with model has only 2 characters
+        //  print them out
+        //  get the count
+
+        int cntOf2CharModel = 0;
+        for (String eachCar : cars) {
+
+            String[] eachCarSplitted = eachCar.split("-");
+            String model = eachCarSplitted[1];
+            // model is second item in splited array (at index 1)
+            //if( eachCarSplitted[1].length() == 2  ) {
+            if (model.length() == 2) {
+                cntOf2CharModel++;
+                //System.out.println(eachCar);
+            }
+
+        }
+        System.out.println("Count Of Cars with only 2 character in their model  = " + cntOf2CharModel);
+
+
+
+
+
 
 
     }
