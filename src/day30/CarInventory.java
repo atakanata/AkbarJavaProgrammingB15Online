260a581faecc57f61b8d32f48763bcdbbc084591
studio2recording <github@cybertekschool.com>
2019-12-18 19:24:07 -0500

    Day 30 : Array Practice : Car Inventory with 8 cars

---
+++ src/day30/CarInventory.java
@@ -0,0 +1,20 @@
+package day30;
+
+public class CarInventory {
+
+    public static void main(String[] args) {
+
+        String[] cars = {"Acura-NSX",
+                "Chevrolet-Corvette",
+                "Chevrolet-Cavalier",
+                "BMW-3 Series",
+                "Pontiac-LeMans",
+                "BMW-7 Series",
+                "Oldsmobile-Achieva",
+                "Honda-Civic"};
+        // Task1 : count Chevrolet and civic
+
+
+    }
+
+}
