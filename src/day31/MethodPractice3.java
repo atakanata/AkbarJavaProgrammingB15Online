fb273433e21f93a038e4a900b9da5e33198bbcb8
studio2recording <github@cybertekschool.com>
2019-12-21 13:51:31 -0500

    DAY 31 : Method Practice 3 : void method practice

---
+++ src/day31/MethodPractice3.java
@@ -0,0 +1,22 @@
+package day31;
+
+public class MethodPractice3 {
+
+    // Create a class called MethodPractice4
+
+    // no need for object when being called                : static
+    // it should be accessible anywhere in your project    : public
+    // it does not return any value                        : void
+    // does not need any external data when being called   : (nothing inside)
+
+    // Create a method called countDownFrom20tillOne
+    // Create a method to print odd numbers from 1-100 : printOddNumbers1to100()
+    // Create a method to spell your name :  Seda -->> S-e-d-a
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
