fb7f02cd6d8d73d4078bbcaab02929738305c741
studio2recording <github@cybertekschool.com>
2019-12-21 17:33:35 -0500

    DAY 31 : Method with parameter Season Checker Method

---
+++ src/day31/SeasonChecker.java
@@ -0,0 +1,47 @@
+package day31;
+
+public class SeasonChecker {
+
+    public static void main(String[] args) {
+
+        decideSeasonAction("Winter");
+        decideSeasonAction("Summer");
+
+        // a String variable declared and assigned value inside main method
+        // It can be accessible in main method
+        String mySeason = "AAAA";
+        decideSeasonAction(mySeason);
+
+    }
+
+    /**
+     * Write a static method called decideSeasonAction
+     * It has one String parameter called season
+     * Inside method method :
+     * according to what user passed it should print correct action
+     */
+    public static void decideSeasonAction(String season) {
+        // this season method parameter can only be accessible inside method body
+        switch (season) {
+
+            case "Spring":
+                System.out.println("Hiking");
+                break;
+            case "Summer":
+                System.out.println("Swimming");
+                break;
+            case "Fall":
+                System.out.println("Pumpkin picking");
+                break;
+            case "Winter":
+                System.out.println("Go Snowboarding");
+                break;
+            default:
+                System.out.println("INVALID SEASON");
+
+        }
+
+    }
+
+
+}
