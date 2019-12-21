24b6d38caf88c5b8ce0bdfbeece973cb4deab80e
studio2recording <github@cybertekschool.com>
2019-12-21 13:41:34 -0500

    DAY 31 : Method Practice 2 : What can i put inside method : ANYTHING

---
+++ src/day31/MethodPratice2.java
@@ -0,0 +1,35 @@
+package day31;
+
+public class MethodPratice2 {
+
+    // write piece of reusable code to count from 1 to 10
+    // give a name count1to10                               : method name
+    // no need for object when being called                : static
+    // it should be accessible anywhere in your project    : public
+    // it does not return any value                        : void
+    // does not need any external data when being called   : (nothing inside)
+
+
+    public static void main(String[] args) {
+
+        count1to10();
+        //System.out.println();
+        count1to10();
+        // optionally you can call using classname.methodName();
+        MethodPratice2.count1to10();
+
+
+    }
+
+    // DOES NOT MATTER WHERE YOU PUT THE METHOD
+    // AS LONG AS IT'S INSIDE THE CLASS { }
+    // AND OUTSIDE ANY OF THE METHOD
+    public static void count1to10() {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+}
