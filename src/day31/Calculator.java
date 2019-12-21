653e4efd12fe97bfea6f168c64e7a4a95b1967a7
studio2recording <github@cybertekschool.com>
2019-12-21 17:49:48 -0500

    DAY 31 : Method with more than one parameters

---
+++ src/day31/Calculator.java
@@ -0,0 +1,30 @@
+package day31;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        // create a static method that add
+        // it accept 2 numbers and print the result of addition
+        add(6, 23);
+        add(16, 3);
+        add(1, 6);
+        add3Numbers(1, 5, 99);
+        add3Numbers(10, 5, 25);
+
+    }
+
+    // a static method that has 2 int parameters
+
+    public static void add(int num1, int num2) {
+
+        System.out.println("addition result " + (num1 + num2));
+
+    }
+
+    public static void add3Numbers(int num1, int num2, int num3) {
+        System.out.println("add 3 numbers result " + (num1 + num2 + num3));
+    }
+
+
+}
