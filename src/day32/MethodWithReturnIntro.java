e5cf8df753c06d676d6236a82fb64ddf0f7b8aeb
studio2recording <github@cybertekschool.com>
2019-12-22 17:26:41 -0500

    DAY 32 : Method with return type : add method with return

--- src/day32/MethodWithReturnIntro.java
+++ src/day32/MethodWithReturnIntro.java
@@ -4,21 +4,34 @@ public class MethodWithReturnIntro {
 
     public static void main(String[] args) {
 
-//       String name =  giveMeMyName();
-//       System.out.println("name = " + name);
-//
-//       int myResult = doubleTheNumber(10) ;
-//       System.out.println("myResult = " + myResult);
+        String name = giveMeMyName();
+        System.out.println("name = " + name);
 
-
+        int myResult = doubleTheNumber(10);
+        System.out.println("myResult = " + myResult);
         // System.out.println( Arrays.toString(arrayObjectHere) ) ;
         // just like we can directly use the toString method above and print the value
         // we can print the result of a method call directly , if the method return a value
         System.out.println("result of doubling 100 is " + doubleTheNumber(100));
+//        System.out.println( doubleTheNumber(5)  );
+//        System.out.println( doubleTheNumber(15)  );
 
+        int add14to16Result = add2Numbers(14, 16);
+        System.out.println("add 14 to 16 Result = " + add14to16Result);
+
+        System.out.println(add2Numbers(14, 16) + 5);
 
     }
 
+    // create a method called add , accept 2 int parameter
+    // and return the result as int
+    //
+    public static int add2Numbers(int num1, int num2) {
+        // int sum = num1+num2;
+        // return sum ;
+        return num1 + num2;
+
+    }
     // doubleTheNumber
     // create a static method that double the value of a number
     // it accept one int parameter and return doubled value of that number
@@ -27,7 +40,7 @@ public class MethodWithReturnIntro {
     // and use it somewhere else
     public static int doubleTheNumber(int num) {
 
-        System.out.println("I am going to double the value of " + num);
+        //System.out.println("I am going to double the value of " + num);
         int result = num * 2;
         return result;
 
---
+++ src/day32/MethodWithReturnTypePractice.java
@@ -0,0 +1,11 @@
+package day32;
+
+public class MethodWithReturnTypePractice {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
