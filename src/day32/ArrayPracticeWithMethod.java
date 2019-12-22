518a619a2cc58d100f9daba4ac34274e9c8468bd
studio2recording <github@cybertekschool.com>
2019-12-22 16:27:32 -0500

    DAY 32 : Method with 2 Array object as parameters

--- src/day32/ArrayPracticeWithMethod.java
+++ src/day32/ArrayPracticeWithMethod.java
@@ -17,6 +17,11 @@ public class ArrayPracticeWithMethod {
         printMinOfIntArray(scores);
         printSumOfIntArray(scores);
 
+        compare2arraySize(new String[]{"Sezgin", "Senay", "Astrit"}, new String[]{"Araz"});
+        String[] names1 = {"Superman", "Batman", "Flash"};
+        String[] names2 = {"Wonder Woman", "Cyborg", "Aquaman"};
+
+        compare2arraySize(names1, names2);
 
     }
 
@@ -67,6 +72,26 @@ public class ArrayPracticeWithMethod {
             sum = sum + each;  //sum+=each ;
         }
         System.out.println("sum = " + sum);
+    }
+
+    /**
+     * compare2arraySize
+     * create a method that accept 2 String array object
+     * and compare the item counts inside these 2 arrays
+     * if arr1 is more then print array 1 has more item
+     * if arr2 is more then print array 2 has more item
+     * else print they have same item count
+     */
+    public static void compare2arraySize(String[] arr1, String[] arr2) {
+
+        if (arr1.length > arr2.length) {
+            System.out.println("array 1 has more item ");
+        } else if (arr1.length < arr2.length) {
+            System.out.println("array 2 has more item ");
+        } else {
+            System.out.println("they have same item count");
+        }
+
     }
     // OPTIONALLY
     // checkScoresAllMoreThan60
