4731bf2895f214e1ff0a7b903995d44cbbcff730
studio2recording <github@cybertekschool.com>
2019-12-22 12:33:23 -0500

    DAY 32 : void method with 2 parameters print alphabet in range

--- src/day32/CharacterActions.java
+++ src/day32/CharacterActions.java
@@ -4,10 +4,50 @@ public class CharacterActions {
 
     public static void main(String[] args) {
 
-        printAtoZ();
-        printZtoA();
+//        printAtoZ();
+//        printZtoA();
+
+        printAlphabetInRange('a', 'f');
+        printAlphabetInRange('G', 'B');
+        printAlphabetInRange('A', 'A');
+        printAlphabetInRange('a', 'A');
+        printAlphabetInRange('Z', 'a');
+
 
     }
+
+    /**
+     * if beginning character is before ending character
+     * for example beginning A , ending D  -->> ABCD
+     * we did similar action with printing A-Z
+     * <p>
+     * if beginning character is after ending character
+     * for example beginning D , ending A  -->> DCBA
+     * we did similar action with printing Z-A
+     */
+    public static void printAlphabetInRange(char beginning, char ending) {
+
+        if (beginning < ending) {
+            System.out.println("WE NEED TO INCREMENT FROM " + beginning + " till " + ending);
+
+            for (char iChar = beginning; iChar <= ending; iChar++) {
+                System.out.print(iChar + " ");
+            }
+            System.out.println();
+
+        } else if (beginning > ending) {
+            System.out.println("WE NEED TO DECREMENT FROM " + beginning + " till " + ending);
+
+            for (char iChar = beginning; iChar >= ending; iChar--) {
+                System.out.print(iChar + " ");
+            }
+            System.out.println();
+
+        } else {
+            System.out.println("THEY ARE SAME CHARACTER!!!!");
+        }
+    }
+
 
     /*
     * printAtoZ
@@ -31,7 +71,7 @@ public class CharacterActions {
     * */
     public static void printZtoA() {
         // this is how we can print in descending order
-        // 'Z'-- -->> 'Y'
+        // 'Z'--  -->> 'Y'
         for (char iChar = 'Z'; iChar >= 'A'; iChar--) {
             System.out.print(iChar + " ");
         }
