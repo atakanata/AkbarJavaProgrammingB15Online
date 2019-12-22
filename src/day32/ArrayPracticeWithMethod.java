57fe376672df92663984741dc16bd826224730ea
studio2recording <github@cybertekschool.com>
2019-12-22 16:28:31 -0500

    DAY 32 : Method with 2 Array object as parameters

--- src/day32/ArrayPracticeWithMethod.java
+++ src/day32/ArrayPracticeWithMethod.java
@@ -85,11 +85,17 @@ public class ArrayPracticeWithMethod {
     public static void compare2arraySize(String[] arr1, String[] arr2) {
 
         if (arr1.length > arr2.length) {
+
             System.out.println("array 1 has more item ");
+
         } else if (arr1.length < arr2.length) {
+
             System.out.println("array 2 has more item ");
+
         } else {
+
             System.out.println("they have same item count");
+
         }
 
     }
