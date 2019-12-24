438d442c3638838210e417f20ca1eaed592caa91
studio2recording <github@cybertekschool.com>
2019-12-23 22:33:14 -0500

    DAY 33 : Method with return type Practice

--- src/day33/MethodWithReturnPractice3.java
+++ src/day33/MethodWithReturnPractice3.java
@@ -24,7 +24,7 @@ public class MethodWithReturnPractice3 {
      *
      * @param birthYear the number that represent birth year
      * @return the calculated value from the age
-     * if the birth year is not within 1900-2020 return 0
+     * if the birth year is not within 1900-2020 then return 0
      */
     public static int calculateAgeFromBirthYear(int birthYear) {
         // I want to take out negative scenario first and just return 0
