1c28009cfcc029bbccf06130316f0467a7cc0949
studio2recording <github@cybertekschool.com>
2019-12-23 22:02:43 -0500

    DAY 33 : return can be used to terminate any method execution before it ends

---
+++ src/day33/ReturnWillTerminateTheMethod.java
@@ -0,0 +1,26 @@
+package day33;
+
+import java.util.Scanner;
+
+public class ReturnWillTerminateTheMethod {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number more than 10 ");
+
+        int num = scan.nextInt();
+        if (num < 10) {
+            // YOU CAN USE return Keyword to get out of the method
+            // in void methods , as long as you don't actually return a value
+            // below line is only thing you can do !!!
+            return;
+        }
+
+        System.out.println("HAPPY HOLIDAYS ");
+        System.out.println("2 DAYS OFF!!!! YAY");
+
+
+    }
+
+}
