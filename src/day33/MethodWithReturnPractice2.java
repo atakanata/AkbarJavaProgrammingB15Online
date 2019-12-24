adf9d9177a783e693fb2baaf440decafdb2f2870
studio2recording <github@cybertekschool.com>
2019-12-23 21:29:06 -0500

    DAY 33 : Get days of the week from arrays of day code

--- src/day33/MethodWithReturnPractice2.java
+++ src/day33/MethodWithReturnPractice2.java
@@ -13,7 +13,20 @@ public class MethodWithReturnPractice2 {
         System.out.println(getDaysOfTheWeekFromNumber(16));
 
         //TODO 2, OPTIONALLY , Create an int Array that has 5 numbers, test your code with those
+        int[] allCodes = {5, 3, 11, 4, 33};
 
+        for (int x = 0; x < allCodes.length; x++) {
+
+            String day = getDaysOfTheWeekFromNumber(allCodes[x]);
+            System.out.println("day = " + day);
+
+        }
+        System.out.println("-------------");
+        for (int eachCode : allCodes) {
+
+            System.out.println("each day = " + getDaysOfTheWeekFromNumber(eachCode));
+
+        }
 
     }
 
@@ -66,5 +79,21 @@ public class MethodWithReturnPractice2 {
 
 //        return dayName;
     }
+    /**
+     * Get Days Of The Week From Number Without Switch Statement
+     * @author Seda
+     * @param daycode
+     * @return
+     */
+//    public static String getDaysOfTheWeekFromNumber2(int daycode){
+//        String [] daysArray={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+//        String day="";
+//        if(daycode>0 && daycode<=7 ){
+//            day=daysArray[daycode-1];
+//        }else{
+//            day="FUNDAY";
+//        }​
+//        return day;
+//    }
 
 }
