d7e5581007984f997eea8f21727b9ab44b79493a
studio2recording <github@cybertekschool.com>
2019-12-27 19:20:38 -0500

    Practice: Methods_ 12/27/2019

--- src/office_hour/Practice_12_27.java
+++ src/office_hour/Practice_12_27.java
@@ -59,3 +59,48 @@ public class Practice_12_27 {
 
 
 }
+
+/*
+class notes:
+12/27/2019
+Practice topic: Methods
+
+methods: grouping a series of statements together to perform a specific action
+
+		methods can make our codes more reusable (don't need to repeat same set of statements)
+
+
+method Decleration:
+
+		access-modfiers  specifiers  return-Type  name(parameters){
+				statements...;
+		}
+
+		during this course:
+			access-mdofiers: public, protected, default, private
+
+			specifiers: static, final, abstract
+
+			return-type(mandatory): void, any  data-types(all primitives or non primitives)
+
+			parameters: any  data-types(all primitives or non primitives)
+
+
+		for Now we will only use public and static for the access midifier and specifier
+
+
+		if return-type is void: does not return any value
+
+		if return type is not void: the method MUST return a value
+									the returning value MUST be matched with the return-type of the method
+
+
+		Methods with parameters: sometime method needs specific information (from the user) to perform its function
+
+			the argument that we are passing MUST match with parameter's data type
+
+
+return statement: it's used for exiting the current method.  we can also use return statement to return a vlaue from the method
+
+
+ */
