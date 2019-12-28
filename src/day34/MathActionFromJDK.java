7f4ef3469ea324e7355962aea16d44cdd25281a8
studio2recording <github@cybertekschool.com>
2019-12-28 13:06:20 -0500

    Day 34 : Method Practice : Using java.lang.Math class methods

---
+++ src/day34/MathActionFromJDK.java
@@ -0,0 +1,29 @@
+package day34;
+
+public class MathActionFromJDK {
+
+    public static void main(String[] args) {
+
+        // where is String class coming from ?
+        // it's coming from a package called java.lang
+        // it's a special package
+        // and any class in it ,does not need importing when being used
+        // for example String class
+
+        // what if I really want to explicitly import String class
+        // import java.lang.String
+
+        //String str = "Hello" ;
+
+        // There is a class under java.lang called Math
+        // and it has lots of math utility methods that ready to be used
+        int sum = Math.addExact(10, 20);
+        System.out.println("sum = " + sum);
+
+        System.out.println("max of 10, 20 --> " + Math.max(10, 20));
+        ;
+
+
+    }
+
+}
