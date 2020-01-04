0ad0ebe18bead673f1cf44c1b8012f04e6dbf7f9
studio2recording <github@cybertekschool.com>
2020-01-04 16:40:26 -0500

    Day 36 : Creating ArrayList Object with data type

---
+++ src/day36/ArrayListIntro.java
@@ -0,0 +1,35 @@
+package day36;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListIntro {
+
+    public static void main(String[] args) {
+
+        // How do we create a ArrayList Object
+
+        ArrayList<String> lst1 = new ArrayList<>();   //  new ArrayList<String>() ;
+        // IT WILL ONLY STORE OBJECT!!! NOT PRIMITIVE
+        //ArrayList<int> lst2 = new ArrayList<>() ;
+
+        // Correct way to create arraylist object that store whole number
+        // is to use Wrapper class type . Primitive type is FORBIDDEN!!!!
+
+        ArrayList<Integer> lst2 = new ArrayList<>(); //  new ArrayList<Integer>() ;
+
+        ArrayList<Long> lst3 = new ArrayList<>(); // new ArrayList<Long>() ;
+
+        ArrayList<Double> lst4 = new ArrayList<>(); // new ArrayList<Double>() ;
+
+        // OPTIONALLY YOU CAN CREATE IN THIS WAY
+
+        // List is a more general data type  compared to ArrayList is more concrete data type
+        // ArrayList is one type of List
+        // Just like Dog is one type of Animal
+        List<String> myLst = new ArrayList<>();
+
+
+    }
+
+}
