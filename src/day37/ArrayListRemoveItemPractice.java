924f7699c30b1225d546400d59326f9fa0eb3542
studio2recording <github@cybertekschool.com>
2020-01-05 17:01:45 -0500

    Day 37 : ArrayList Practice : removing element

--- src/day37/ArrayListRemoveItemPractice.java
+++ src/day37/ArrayListRemoveItemPractice.java
@@ -17,10 +17,17 @@ public class ArrayListRemoveItemPractice {
 
 //        lst.remove(3);
 //        System.out.println("lst after removing index 3 item = " + lst);
-//        lst.remove(2);
+        lst.remove(2);
         // I want to remove item by it's value rather than index
-        //       lst.remove( new Integer(2)  );
-        lst.remove(Integer.valueOf(2));
+        //lst.remove( new Integer(2)  );
+
+        // if you try to delete an item that does not exist by value
+        // IT WILL DO NOTHING
+        lst.remove(Integer.valueOf(112));
+
+        // if you try to delete using the index list don't have
+        // IT WILL throw IndexOutOfBoundsException
+        //lst.remove(112);
         System.out.println("lst = " + lst);
 
 
---
+++ src/day37/MethodOverloadingWithWrapper.java
@@ -0,0 +1,24 @@
+package day37;
+
+public class MethodOverloadingWithWrapper {
+
+    public static void main(String[] args) {
+
+
+        printNum(100);
+        printNum(Integer.valueOf(100));
+
+
+    }
+
+
+    public static void printNum(Integer x) {
+        System.out.println("printing Integer Object x = " + x);
+    }
+
+
+    public static void printNum(int x) {
+        System.out.println("printing primitive int x = " + x);
+    }
+
+}
