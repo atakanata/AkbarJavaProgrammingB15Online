18f0159b5d9c8b0c0d41c641d4a3ed3ac79d9b46
CybertekSchool <33201481+CybertekSchool@users.noreply.github.com>
2020-01-05 22:28:04 -0500

    Day 37 : ArrayList Methods Practice Day

--- src/day37/ArrayListCreationShortWay.java
+++ src/day37/ArrayListCreationShortWay.java
@@ -5,22 +5,23 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * @author : akbar
+ * @author : Akbar
  * Created At : 1/5/20
  */
 public class ArrayListCreationShortWay {
 
     public static void main(String[] args) {
 
-        List<String> superHeros = new ArrayList<>();
-        superHeros.add("Superman");
-        superHeros.add("Batman");
-        superHeros.add("Cyborg");
-        superHeros.add("Flash");
-        superHeros.add("Aquaman");
-        superHeros.add("Wonder Woman");
+        List<String> superHeroes = new ArrayList<>();
+        superHeroes.add("Superman");
+        superHeroes.add("Batman");
+        superHeroes.add("Cyborg");
+        superHeroes.add("Flash");
+        superHeroes.add("Aquaman");
+        superHeroes.add("Wonder Woman");
 
-        List<String> superHeroCopy = new ArrayList<>( superHeros );
+        // Copying everything inside superHeroes list into superHeroCopy lst
+        List<String> superHeroCopy = new ArrayList<>( superHeroes );
         System.out.println("superHeroCopy = \n\t" + superHeroCopy);
 
         // THIS WILL CREATE A UNMODIFIABLE LIST !! no adding or removing (like array again?? yee )
@@ -38,6 +39,17 @@ public class ArrayListCreationShortWay {
         topicsCopy.add("Interview Prep"); //GOOD!!
         System.out.println("topicsCopy after adding item = \n\t" + topicsCopy);
 
+        // After Java 9  Arrays.asList(...) has been improved by List.of(...)
+        // It's important to know Arrays.asList(...) because lots of company still using Java 8
+        // and yes this is the simplest way to
+        // create a type of unmodifiable(non-resizable) List Object (not ArrayList)
+        List<String> oop = List.of("Abstraction","Encapsulation","Inheritance","Polymorphism") ;
+        System.out.println("oop = " + oop);
+        // and again if we want to save it into a regular ArrayList Object we can do :
+        List<String> oopCopy = new ArrayList<>( oop ) ;
+        // or in one shot
+        List<String> oopLst = new ArrayList<>( List.of("Abstraction","Encapsulation","Inheritance","Polymorphism") ) ;
+        System.out.println("oopLst in one short= " + oopLst);
 
 
     }
