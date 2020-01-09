21235bacea895b0d5e161d14d83c54e708bc3519
studio2recording <github@cybertekschool.com>
2020-01-08 20:19:57 -0500

    DAY 38 : Add All methods for Shopping List

--- src/day38/ArrayListBulkOperation.java
+++ src/day38/ArrayListBulkOperation.java
@@ -28,22 +28,6 @@ public class ArrayListBulkOperation {
         System.out.println("new Lst with all items = " + newLst);
 
 
-        ArrayList<Integer> nums1 = new ArrayList<>();
-        nums1.add(123);
-        nums1.add(100);
-        nums1.add(223);
-        nums1.add(133);
-        nums1.add(125);
-        nums1.add(122);
-
-        ArrayList<Integer> nums2 = new ArrayList<>();
-        nums2.add(2000);
-        nums2.add(1000);
-
-        // add all nums2 items into nums1
-        nums1.addAll(nums2);
-        System.out.println("nums1 = " + nums1);
-        System.out.println("nums2 = " + nums2);
 
 
     }
