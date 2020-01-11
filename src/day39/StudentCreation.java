214fb1451e9ed0a174dd5d2064e7e55168f2c5aa
studio2recording <github@cybertekschool.com>
2020-01-11 16:34:22 -0500

    Day 39 : Creating Objects out of Your Own Class| variable category

--- src/day39/StudentCreation.java
+++ src/day39/StudentCreation.java
@@ -19,8 +19,43 @@ public class StudentCreation {
         s2.gender = 'M';
 
         System.out.println("s2 age " + s2.age);
+        // I want to save Juma's afe before I change so i still have access
 
+        int oldAge = s2.age;
+
+        s2.age = 28;
+        System.out.println(s2.name + " | " + s2.age + " | " + s2.gender);
+
+        System.out.println("Juma's old Age = " + oldAge);
+        // tell me the different between old age and new age
+
+        System.out.println("The differerence is " + (oldAge - s2.age));
 
     }
 
 }
+/*
+*
+Variable types :
+according to what kind of data it store :
+	primitive variable -->> pure value
+	reference variable -->>
+					the pointer to the object in the heap
+					the address of the obejct in the heap
+					remote of the TV
+					leash of the dog
+
+according to the location it shows up :
+
+	 inside method : --> local variable
+	 inside ( ) of method header -->>
+	 				method parameter
+
+	 outside method inside class
+	 		--> Field
+	 			instance field or instance variable
+	 			static filed (will learn later)
+
+
+
+* */
