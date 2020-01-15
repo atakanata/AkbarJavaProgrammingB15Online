4049019942b586ac821cab1129ae19926ac2b2c1
studio2recording <github@cybertekschool.com>
2020-01-14 21:07:09 -0500

    Day 40 : public , (default) private access modifier | Encapsulation to protect field.

---
+++ src/day41/Person.java
@@ -0,0 +1,37 @@
+package day41;
+
+public class Person {
+
+    // private make it only accessible within the same class
+    private String name;
+    private int age;
+    private long ssn;
+
+    // what if I want to only get the name
+    public String getName() {
+        return name;
+    }
+
+    // what if I want to only change the name
+    public void setName(String newName) {
+        name = newName;
+    }
+
+
+    // write a method to give value to the filed
+    public void setAll(String newName, int newAge, long newSsn) {
+        // YOU CAN PUT WHATEVER CHECK HERE TO MAKE SURE
+        // THE DATA IS CORRECT or any other logic
+        name = newName;
+        age = newAge;
+        ssn = newSsn;
+    }
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", ssn=" + ssn +
+                '}';
+    }
+}
