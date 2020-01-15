15c7af58e54eed9abc43fb41fdfd63406baaa8e9
studio2recording <github@cybertekschool.com>
2020-01-14 22:05:33 -0500

    Day 40 : This keyword and generating getters and setters

--- src/day41/Product.java
+++ src/day41/Product.java
@@ -5,6 +5,11 @@ package day41;
 // provide public getters and setters method
 public class Product {
 
+    // this keyword can be used to refer the current object
+    // we are working on
+    // we can use it to refer the instance field
+    // this.fieldName
+    // only can be used inside instance method (and later constructor)
     private String name;
     private double price;
 
@@ -12,21 +17,40 @@ public class Product {
         return name;
     }
 
-    public void setName(String newName) {
-        name = newName;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double newPrice) {
-
-        if (newPrice > 0) {
-            price = newPrice;
-        }
-
+    public void setPrice(double price) {
+        this.price = price;
     }
+
+    //    public String getName() {
+//
+//        return this.name;
+//        //return name;
+//    }
+//
+//    public void setName(String name) {
+//        //name = newName;
+//        this.name = name ;
+//    }
+//
+//    public double getPrice() {
+//        return this.price;
+//    }
+//
+//    public void setPrice(double price) {
+//
+//        if (price > 0) {
+//            this.price = price;
+//        }
+//
+//    }
 
 
 }
