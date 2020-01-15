6a84ec33bdacb5aeba8ece538e0db4ea845174aa
studio2recording <github@cybertekschool.com>
2020-01-15 18:54:39 -0500

    Day 42 : OFFICE HOUR : GETTERS AND SETTERS , this keyword practice with note

--- src/office_hour/offer_hour_01_15/TeacherTest.java
+++ src/office_hour/offer_hour_01_15/TeacherTest.java
@@ -17,3 +17,77 @@ public class TeacherTest {
     }
 
 }
+/*
+
+
+----------------------
+
+Class and Object
+
+Class is a template for Object
+it can define what kind of attribute and behaviour an object should have
+
+attrinute : -->> instance variables | fields
+behaviour : -->> instance methods
+
+Object is the actual thing created out of this template
+and any object created out of same template will have the attribute and behaviours defined in the template
+
+
+Protecting the data|attribute of the object
+we can encapsulate the instance field by making it private and provide public getters and setters
+
+we have 4 access modifier : public , protected , default , private
+(IGNORE protected for now until inheritance topic comes in )
+
+public -->> accessible anywhere
+default -->> no access modifer
+		--> only within the package , also known as package private
+private -->> only accessible within the same class
+
+
+outside of the class when we create object we can give a object any name we want , like p1, p2  or any other name
+
+if we want to refer the object in the template itself , we can use the keyword -->> this --> the current object we are working on
+
+so outside the template it would look like  :
+	Person p1 = new Person() ;
+	p1.age = 18 ;
+	p1.eat() ;
+
+inside your template it would look like  :
+	this.age -->> to point to the instance field age
+	this.name -->> to point to the instance field name
+	this.eat();  // optional , usually it will just work without
+
+
+
+public class Person{
+
+	private int age ;
+	private String name;
+
+	public int getAge(){
+		return age ; // return this.age;
+	}
+	public void setAge(int age){
+		this.age = age ;
+	}
+	public String getName(){
+		return name; // return this.name;
+	}
+	public void setName(String name){
+		// BUNCH OF VALIADITON I CAN USE HERE ....
+		this.name = name ;
+	}
+
+
+	public void eat(){
+		print(name + "eating") ;
+	}
+
+
+}
+
+
+* */
