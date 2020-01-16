e0e1bd9fb086126ba00a379ea6eeb963505b1438
studio2recording <github@cybertekschool.com>
2020-01-15 22:01:32 -0500

    Day 42 : Constructor Practice

--- src/day42/Movie.java
+++ src/day42/Movie.java
@@ -2,19 +2,35 @@ package day42;
 
 public class Movie {
 
-    String name;
-    double length;
-    String type;
+    private String name;
+    private double length;
+    private String type;
 
-    public Movie() {
-        System.out.println("EMPTY MOVIE");
-    }
+
+    // default constructor :
+    // a constructor that give by compiler
+    // only if you don't have any constructor in your class
+    // it's invisible
+    // and it has no parameter
+    // it has no code inside
 
     // write a constructor to set all the fields value
     public Movie(String name, double length, String type) {
         this.name = name;
         this.length = length;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public String getType() {
+        return type;
     }
 
     // toString method so we can directly print out
---
+++ src/day42/MovieAction.java
@@ -0,0 +1,30 @@
+package day42;
+
+public class MovieAction {
+
+
+    public static void main(String[] args) {
+
+
+//        Movie m1 = new Movie();
+//        System.out.println("m1 = " + m1);
+
+        Movie m2 = new Movie("Joker", 2.2, "Drama");
+        Movie m3 = new Movie("Frozen2", 1.5, "Family");
+        Movie m4 = new Movie("Pursuit of Happiness", 2.0, "Drama");
+
+        System.out.println("m2 = " + m2);
+        System.out.println("m3 = " + m3);
+        System.out.println("m4 = " + m4);
+
+        // I WANT TO GET THE name OF M2 SEPARATELY SO I CAN DO SOME STUFF
+        // I can not access the field directly because it's private
+        // i need a getter method to access
+        // System.out.println(m2.name);
+        System.out.println(m2.getName());
+
+
+    }
+
+
+}
