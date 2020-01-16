542ef2ddbeae0172df7c4a5ee424c35fcb7dc93b
studio2recording <github@cybertekschool.com>
2020-01-15 21:03:10 -0500

    Day 42 : Constructor Intro

---
+++ src/day42/Bike.java
@@ -0,0 +1,35 @@
+package day42;
+
+public class Bike {
+
+    int speed;
+    int gear;
+
+    // constructor is a block of code
+    // that has same name as classname
+    // and has no return type
+    // if it does not meet above requirement
+    // it will not be a constructor
+    // the only time you can call this is :
+    // when object is being created
+
+    public Bike() {
+        System.out.println("message from constructor");
+        gear = 1;
+    }
+
+    public Bike(int newGear) {
+        gear = newGear;
+        // this.gear = newGear ;
+    }
+
+
+    public Bike(int gear, int speed) {
+
+        this.gear = gear;
+        this.speed = speed;
+
+    }
+
+
+}
