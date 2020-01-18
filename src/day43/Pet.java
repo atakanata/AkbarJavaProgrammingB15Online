428b332e8ff4619266e5ece7567a23e4eec395e6
studio2recording <github@cybertekschool.com>
2020-01-18 16:52:40 -0500

    Day 43 : Pet Class for Pet objects

---
+++ src/day43/Pet.java
@@ -0,0 +1,65 @@
+package day43;
+
+public class Pet {
+
+    private String type;
+    private String name;
+
+    public Pet() {
+        this.type = "unknown";
+        this.name = "no-name";
+    }
+
+    public Pet(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public void speak() {
+
+        //switch (type.toLowerCase()  ){
+        switch (type) {
+
+            case "cat":
+                System.out.println("MEOW");
+                break;
+            case "dog":
+                System.out.println("WOOF");
+                break;
+            case "horse":
+                System.out.println("NEIGH");
+                break;
+            case "cow":
+                System.out.println("MOOO");
+                break;
+            default:
+                System.out.println("UNKNOWN ANIMAL!");
+
+        }
+
+    }
+
+    public String toString() {
+        return "Pet{" +
+                "type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
