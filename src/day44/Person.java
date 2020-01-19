9ba074a205175c37447ee54d4210507267e6b868
studio2recording <github@cybertekschool.com>
2020-01-19 12:40:58 -0500

    Day 44 : Person class , static field added

--- src/day44/Person.java
+++ src/day44/Person.java
@@ -2,15 +2,16 @@ package day44;
 
 public class Person {
 
+    // instance field : belong to the instance(object)
+    // every Person object will get their own name and age
+    // changing the value of one object does not affect another
+    private String name;
+    private int age;
     // static field : belong to the class (type itself)
     // there is only one copy of this value shared by all the objects
     // created out of this Person template|blue print
     public static String species;
-    private String name;
-    // instance field : belong to the instance(object)
-    // every Person object will get their own name and age
-    // changing the value of one object does not affect another
-    private int age;
+
 
     public Person(String name, int age) {
         this.name = name;
