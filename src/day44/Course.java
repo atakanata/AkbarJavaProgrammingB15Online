7f64a780cec49af086e3d613d5a3a0d7ef09dcf7
studio2recording <github@cybertekschool.com>
2020-01-19 13:17:24 -0500

    Day 44 : Course Class with static counter to count objects

--- src/day44/Course.java
+++ src/day44/Course.java
@@ -2,7 +2,9 @@ package day44;
 
 public class Course {
 
+    // there is only one value for this counter shared by all the objects
     static int counter;
+    // each object will have their own name and type
     String name;
     String type;
 
@@ -13,7 +15,6 @@ public class Course {
         ++counter;
         // You can access static field
         // in constructor, instance method , static methods
-
     }
 
 
