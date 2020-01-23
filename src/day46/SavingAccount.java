a1520d251221e7dc5dff7dc68911d83feae6456f
studio2recording <github@cybertekschool.com>
2020-01-22 20:38:16 -0500

    Day 46 : Bank Account : Method Overriding | super.withdraw(amount+30)

--- src/day46/SavingAccount.java
+++ src/day46/SavingAccount.java
@@ -1,46 +1,40 @@
 package day46;
 
 public class SavingAccount extends BankAccount {
-
-    //    String accountHolder;
+//    String accountHolder;
 //    long accountNum;
 //    double balance;
     double interestRate;
 
-
     public SavingAccount(String accountHolder, long accountNum, double balance, double interestRate) {
-
         super(accountHolder, accountNum, balance);
         this.interestRate = interestRate;
-
     }
 
     // BAD IDEA TO HAVE MAIN HERE , JUST DOING IT ANYWAY
     public static void main(String[] args) {
 
-        SavingAccount s1 = new SavingAccount("Mohammed", 12345, 50000, 4.2);
+        SavingAccount s1 = new SavingAccount("Mohammed", 12345, 500, 4.2);
         System.out.println("s1 = " + s1.toString());
-
-        s1.withdraw(10000);
+        s1.withdraw(100);
         System.out.println("s1 = " + s1);
-
 
     }
 
-    // in our SavingAccount , if we withdraw amount you withdraw
-    // you get 30 dollar penalty
+    // in our SavingAccount , if you withdraw any amount of money
+    // you will get 30 dollar penalty
     // so we will override the withdraw method to reflect this requirement
     @Override
     public void withdraw(int amount) {
         // taking away 30$ more on top of what you withdraw
-        //balance -= 30 ;
         //balance = balance - amount -30 ;
-        balance -= amount;
-        balance -= 30; // this is the penalty
+//        balance -= amount;
+//        balance -= 30; // this is the penalty
+        super.withdraw(amount);
+        super.withdraw(30);
+        // or super.withdraw(amount+30);
 
     }
-
-
     @Override // its optional , once being used , it will enforce overriding rule
     // if any rule does not match it will not even compile
     // so it's always good to use it to prevent accidental errors
