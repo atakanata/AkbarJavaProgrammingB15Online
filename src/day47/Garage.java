bf42ac8f7fec26dfe5c711aaf4a873b94d779111
studio2recording <github@cybertekschool.com>
2020-01-25 16:58:20 -0500

    Day 47 : Creating Object and testing in Garage in main method

--- src/day47/Garage.java
+++ src/day47/Garage.java
@@ -2,15 +2,29 @@ package day47;
 
 public class Garage {
 
-
     public static void main(String[] args) {
-
         // Abstract class can not be instantiated
         // It means we can not create object out of it
-//        Car c1 = new Car();
+//        Car c = new Car();
 
+        ElectricCar c1 = new ElectricCar();
+        c1.start();
+        c1.goBackward();
+        c1.goForward();
+        c1.turn("Right");
+
+        // Create another sub class of Car called GasCar
+        // add a instance field called gasLevel as int
+        // implements all the abstract method of Car class
+
+        // Create GasCar object here
+        // call all the methods to test
+        GasCar c2 = new GasCar();
+        c2.start();
+        c2.goBackward();
+        c2.goForward();
+        c2.turn("Left");
 
     }
-
 
 }
