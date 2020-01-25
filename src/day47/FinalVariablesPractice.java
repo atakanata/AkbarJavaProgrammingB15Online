f81b4e444169e25b36e963bee1e7555ab916b4ab
studio2recording <github@cybertekschool.com>
2020-01-25 15:26:17 -0500

    Day 47 : final variables(local variables, fields, method parameters)

--- src/day47/FinalVariablesPractice.java
+++ src/day47/FinalVariablesPractice.java
@@ -34,12 +34,20 @@ public class FinalVariablesPractice {
         // can not reassign value because it's final
 //        Student.school = "abc" ;
 
+        // INTELLIJ AUTOMATICALLY RECOGNIZE CONSTANT (public static final field)
+        // it will automatically make it italic (This can not be done by programmer)
+        // easy way to distinguish between fields and methods is
+        // by looking at () at the end
+
         System.out.println(Byte.MAX_VALUE);
 
         // PI in Java
         System.out.println(Math.PI);
 
         printDoubledNumber(12);
+
+        final String name = "Michael";
+        // name = "Zehra";  this will not work , can not be reassigned
 
     }
 
