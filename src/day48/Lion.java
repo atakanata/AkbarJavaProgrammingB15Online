8c5c4207288ab5d7f28f10ea4f704759bc8c3c2f
studio2recording <github@cybertekschool.com>
2020-01-26 13:58:28 -0500

    Day 48 : What Can I do with abstract type : GENERILIZE | Animal a1 = new Lion();

--- src/day48/Lion.java
+++ src/day48/Lion.java
@@ -1,14 +1,28 @@
 package day48;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lion extends Mammals {
 
     public static void main(String[] args) {
 
-        Lion l1 = new Lion();
-        l1.drinkMilk();
-        l1.makeNoise();
+//        Lion l1 = new Lion();
+//        l1.drinkMilk();
+//        l1.makeNoise();
+
+        // any class you created , it can be a data type for a variable
+        // also known as reference variable
+
+
+        Animal a1 = new Lion();
+        a1.makeNoise();
+
+        List<Integer> lst = new ArrayList<>();
+
 
     }
+
 
     @Override
     public void drinkMilk() {
