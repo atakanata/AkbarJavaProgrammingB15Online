d16e5cfb0b1eaf6a61bfc11dbf15074f4d2f5460
studio2recording <github@cybertekschool.com>
2020-01-26 17:01:32 -0500

    Day 48 : interface default behaviours

--- src/day48/Bouncible.java
+++ src/day48/Bouncible.java
@@ -1,8 +1,19 @@
 package day48;
 
+
 public interface Bouncible {
 
-    public static final double GRAVITY = 9.81;
-    public abstract void bounce();
+    // ALL THE MEMBERS IN INTERFACE MUST BE PUBLIC
+    // THIS IS EXCLUSIVE TO INTERFACE TYPE
+    // NOWHERE ELSE YOU GET THIS AT ALL!!!!!!!!!!!!!!!!!!!!!!
+    // if we don't add any part of public static final for the field
+    // all interface fields are automatically public static final
+    double GRAVITY = 9.81;
 
+    // public static final double GRAVITY = 9.81;
+    // every member of interface automatically public
+    // if no access modifier is given
+    // all the method with no body automatically public asbtract
+    //void bounce();
+    public abstract void bounce();
 }
