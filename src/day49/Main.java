cc3178c86d65828028ec5b197813f723ff10e0b1
studio2recording <github@cybertekschool.com>
2020-01-28 21:01:40 -0500

    Day 49 : Calling Interface static methods(NOT INHERITED!!!)

--- src/day49/Main.java
+++ src/day49/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        // calling interface static method
+        // we can use static way
+        // interfaceName.staticMemberName
+        // no implementing needed!!!!
         Juicy.squeeze();
 
 
