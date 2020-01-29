d595eb609789a08d11685b82cb214b088f2fc779
studio2recording <github@cybertekschool.com>
2020-01-28 21:00:29 -0500

    Day 49 : Interface static methods

--- src/day49/Juicy.java
+++ src/day49/Juicy.java
@@ -1,7 +1,26 @@
 package day49;
 
+// Interface can achieve loosely coupling between implementing classes
+// for example : When we say flyable
+// all the implementing classes like Plane , Insects, Superman ....
+// can be all Flyable and has nothing to do with each other
+// and almost no knowledge about each other : known as loosely coupling
+// This is why it's always preferred in design
+
+
 public interface Juicy {
 
     public abstract void melt();
+
+    // another type of method Interface can have is :
+    // static method , it must have a body
+    // and it's there to provide some utility methods
+    // that does not belong to any class
+    // STATIC METHODS of interface ARE NOT INHERITED!!!!!!
+    // IT MUST HAVE A BODY !!!!!!!!
+    public static void squeeze() {
+        System.out.println("static squeezing");
+    }
+
 
 }
---
+++ src/day49/Main.java
@@ -0,0 +1,15 @@
+package day49;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        Juicy.squeeze();
+
+
+    }
+
+
+}
