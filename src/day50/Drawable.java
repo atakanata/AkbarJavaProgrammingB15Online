13d8b68a0a3616e79984dff60182a652b5920c28
studio2recording <github@cybertekschool.com>
2020-01-29 19:45:30 -0500

    Day 50 : Drawable interface , Shape Abstract class

---
+++ src/day50/Drawable.java
@@ -0,0 +1,8 @@
+package day50;
+
+public interface Drawable {
+
+    // public abstract is automatically assumed for a method without a body
+    public abstract void draw();
+
+}
---
+++ src/day50/Shape.java
@@ -0,0 +1,38 @@
+package day50;
+
+/*
+* Task 2
+		Create an Interface called Drawable
+				it has draw method
+		Create an abstract class called Shape
+		it implements Drawable interface
+		it has 2 fields :  String name , double area
+		abstract method calculateArea();
+		Create 2 concrete Shape classes called
+				Square
+					field : length
+					instance methods : (implement all abstract methods)
+					toString method
+				Cirlce
+					field : radius
+					instance methods : (implement all abstract methods)
+					toString method
+* */
+// abstract class is not required to implement
+// inherited abstract methods of the interface
+public abstract class Shape implements Drawable {
+
+    String name;
+    double area;
+
+    // why abstract class have constructor even we can not use it to create an object ?
+    // it exists purely to let sub class to call and reuse the functionality in sub class constructor
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public abstract void calculateArea();
+//    public abstract void draw();
+
+}
