e73be702866a69fb80fed4d63651945454be4d90
studio2recording <github@cybertekschool.com>
2020-02-01 16:12:00 -0500

    Day 51 : String implements Comparable interface so it has comparedTo method

---
+++ src/day51/ComparableString.java
@@ -0,0 +1,21 @@
+package day51;
+
+public class ComparableString {
+
+
+    public static void main(String[] args) {
+
+
+        String s1 = "abc";
+        String s2 = "xyz";
+
+        // the only thing matter is - or +  or 0
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s2.compareTo(s1));
+        System.out.println(s1.compareTo(s1));
+
+
+    }
+
+
+}
