2943ab2b6d421ddb721df7847d81f52791afe9f7
studio2recording <github@cybertekschool.com>
2020-02-02 11:54:33 -0500

    Day 52 : Warm up , Check Account name isPalindrome or not

--- src/day52/warmup/Account.java
+++ src/day52/warmup/Account.java
@@ -21,7 +21,6 @@ public class Account implements Transferable {
 //        // or optionally use the method
 //        otherAccount.deposit( balance);
 //        withdraw( balance );
-
     }
 
     public int getBalance() {
@@ -43,6 +42,22 @@ public class Account implements Transferable {
                 ", balance=" + balance +
                 '}';
     }
+
+    // add a method called isPalindrome and return true if the name is palindrome
+    // if a String is still same String after reversing it , it is regarded as Palindrome
+    public boolean isPalindrome() {
+
+        // first store it into temp and replace all the space and make it lowercase
+        String nameCopy = this.name.toLowerCase().replace(" ", "");
+        // we need to reverse the value then check whether they are still same String or not
+        String reverseResult = "";
+        for (int i = nameCopy.length() - 1; i >= 0; i--) {
+            reverseResult += nameCopy.charAt(i);
+        }
+
+        return nameCopy.equals(reverseResult);
+    }
+
 
 }
 /*
--- src/day52/warmup/BankTest.java
+++ src/day52/warmup/BankTest.java
@@ -4,6 +4,20 @@ public class BankTest {
 
     public static void main(String[] args) {
 
+        Account a1 = new Account("John Snow", 10000);
+        Account a2 = new Account("Arya Stark", 7000);
+
+        System.out.println("a1 before = " + a1);
+        System.out.println("a2 before  = " + a2);
+
+        a1.transferAll(a2);
+        System.out.println("a1 after =  " + a1);
+        System.out.println("a2 after = " + a2);
+
+        a2.transferAll(a1);
+        System.out.println("a1 after 2nd transfer =  " + a1);
+        System.out.println("a2 after 2nd transfer =  " + a2);
+
 
     }
 
