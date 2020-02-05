8dff564f643bd95051e7674e6b9f8eead92c592d
studio2recording <github@cybertekschool.com>
2020-02-04 19:32:25 -0500

    Day 53 : Polymorphism Practice : Orange

---
+++ src/day53/Orange.java
@@ -0,0 +1,29 @@
+package day53;
+
+public class Orange extends Fruit {
+
+    int vcLevel;
+
+    public Orange(String taste, String color, int vcLevel) {
+        super(taste, color);
+        this.vcLevel = vcLevel;
+    }
+
+    @Override
+    public void getDigested() {
+
+        System.out.println("Orange with Vitamin C level" + vcLevel
+                + " has color " + color
+                + " and it has " + taste + " taste when you digest");
+
+    }
+
+    @Override
+    public String toString() {
+        return "Orange{" +
+                "vcLevel=" + vcLevel +
+                ", taste='" + taste + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
