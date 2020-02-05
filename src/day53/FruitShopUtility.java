f97f309ca02754d31e63b8e4aeebd628f5cbb9c9
studio2recording <github@cybertekschool.com>
2020-02-04 21:14:28 -0500

    Day 53 : Polymorphism Practice :Polymorphic method parameter

---
+++ src/day53/FruitShopUtility.java
@@ -0,0 +1,29 @@
+package day53;
+
+public class FruitShopUtility {
+
+
+    public static void main(String[] args) {
+
+
+        Fruit f1 = new Apple("crispy but tasteless", "hot red", "Gala");
+        Fruit f2 = new Orange("sour", "orange", 12);
+
+
+    }
+
+    // displayFruit
+    public static void displayFruit(Fruit anyFruit) {
+
+        System.out.println("Displaying fruit \n" + anyFruit.toString());
+
+    }
+
+
+    // sellFruit
+
+
+    // buyFruit
+
+
+}
