07ff9661b7ddcfffaa4b80c9bf2efa168eef4843
studio2recording <github@cybertekschool.com>
2020-02-04 22:00:12 -0500

    Day 53 : Polymorphic method parameter and return type

--- src/day53/FruitShopUtility.java
+++ src/day53/FruitShopUtility.java
@@ -5,6 +5,7 @@ public class FruitShopUtility {
 
     public static void main(String[] args) {
 
+        Fruit fx = null;  //new Orange("sour", "orange", 12);// new Apple("crispy but tasteless", "hot red", "Gala");
 
         Fruit f1 = new Apple("crispy but tasteless", "hot red", "Gala");
         Fruit f2 = new Orange("sour", "orange", 12);
@@ -15,11 +16,38 @@ public class FruitShopUtility {
         displayFruit(new Apple("tasteless", "red", "Golden"));
 
         System.out.println("----------digest better call ----------------");
-        digestBetter(f2);
-        digestBetter(new Orange("sweet", "yellowish", 1));
+//        digestBetter(f2);
+////        digestBetter(new Orange("sweet", "yellowish", 1));
 
+        System.out.println("----------getMyFavoriteFruit call ----------------");
+        Fruit x = getMyFavoriteFruit();
+        x.getDigested();
+        System.out.println("B15 Favorite Fruit = " + x);
+
+        System.out.println("----------getFavoriteByType call ----------------");
+        System.out.println("getFavoriteByType(1) = " + getFavoriteByType(1));
+        System.out.println("getFavoriteByType(2) = " + getFavoriteByType(2));
+        System.out.println("getFavoriteByType(2) = " + getFavoriteByType(3));
 
     }
+
+
+    // Create a static method called GetFavoriteByType
+    // it has one int parameter
+    // return favorite apple object if type is 1 , orange object if type 2
+    public static Fruit getFavoriteByType(int type) {
+
+        if (type == 1) {
+            return new Apple("Sweet", "blue", "Ginger");
+        } else if (type == 2) {
+            return new Orange("cripsy", "green", 100);
+        } else {
+            // we can return null for any method that return reference type
+            // because null can be assigned to a reference type variable
+            return null;
+        }
+    }
+
 
     // Create a static method called getMyFavoriteFruit
     // accept no parameter and return your Favorite Fruit Object
