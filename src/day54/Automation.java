5bf148e190722f4beda6d88309470260b7be3ea9
studio2recording <github@cybertekschool.com>
2020-02-05 20:04:23 -0500

    Day 54 : Polymorphism Usage in Selenium : Aha!

--- src/day54/Automation.java
+++ src/day54/Automation.java
@@ -4,12 +4,14 @@ public class Automation {
 
     public static void main(String[] args) {
 
+        System.out.println("---Running on Chrome");
         WebDriver c1 = new ChromeDriver();
         c1.openBrowser();
         c1.navigateTo("www.cybertekschool.com");
         c1.maximize();
         c1.closeBrowser();
 
+        System.out.println("---Running on Firefox");
         c1 = new FirefoxDriver();
         c1.openBrowser();
         c1.navigateTo("www.cybertekschool.com");
