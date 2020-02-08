fa6e9316e0c101516c81bc41707c922e321c4b62
studio2recording <github@cybertekschool.com>
2020-02-08 14:10:08 -0500

    day 55 : Upcasting and Downcasting

--- src/day55/Casting.java
+++ src/day55/Casting.java
@@ -19,7 +19,7 @@ public class Casting {
         Dog d = (Dog) o; // this is downcasting from type Object to Dog
         d.bark();
 
-        // in order to let the Dog object bark , we need to refer a dog as a Dog or we don't have access to Dog only action
+        // Creating object with Object reference
         Object o2 = new Object();
         // compile fine because Dog IS-A Object , BUT CLASS CAST EXCEPTION AT RUNTIME
         // Because d2 = new Object() will NOT WORK!!!
