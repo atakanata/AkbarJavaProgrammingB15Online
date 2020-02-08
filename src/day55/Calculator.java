bb694825a9938c70fcc0c850c5bab8dd53ca07ef
studio2recording <github@cybertekschool.com>
2020-02-08 12:06:25 -0500

    day 55 : returning this (to return same object used to call the method)

--- src/day55/Calculator.java
+++ src/day55/Calculator.java
@@ -4,5 +4,28 @@ public class Calculator {
 
     int result;
 
+    public static void main(String[] args) {
+
+        Calculator c1 = new Calculator();
+//        c1.addNum(100);
+        c1.addNum(100).addNum(200).addNum(100);
+//        System.out.println(c1.result);
+//        c1 = c1.addNum(100);
+//        c1 = c1.addNum(200);
+//        c1 = c1.addNum(100);
+
+
+    }
+
+    public Calculator addNum(int num) {
+
+        this.result += num;
+//        Calculator c = new Calculator();
+//        return c;
+        // it will return the same object you used to call this method
+        // it will allow method chaining using same object
+        return this;
+    }
+
 
 }
