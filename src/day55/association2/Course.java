8d3908120a87f95a4351682f01f3c7473ba5f670
studio2recording <github@cybertekschool.com>
2020-02-08 16:52:39 -0500

    day 55 : Course and Students association

---
+++ src/day55/association2/Course.java
@@ -0,0 +1,22 @@
+package day55.association2;
+
+import java.util.List;
+
+public class Course {
+
+    String subject;
+    List<String> studentNames;     ////String[] studentNames;  this will work but fixed in size
+
+    public Course(String subject, List<String> studentNames) {
+        this.subject = subject;
+        this.studentNames = studentNames;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "subject='" + subject + '\'' +
+                ", studentNames=" + studentNames +
+                '}';
+    }
+}
---
+++ src/day55/association2/CourseRunner.java
@@ -0,0 +1,4 @@
+package day55.association2;
+
+public class CourseRunner {
+}
