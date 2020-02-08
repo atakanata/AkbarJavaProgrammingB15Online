f4ee5b14884e7beef70c25abd15417c50a3358dd
studio2recording <github@cybertekschool.com>
2020-02-08 12:10:52 -0500

    day 55 : returning this (to return same object used to call the method)

--- src/day55/Calculator.java
+++ src/day55/Calculator.java
@@ -4,24 +4,21 @@ public class Calculator {
 
     int result;
 
-    public static void main(String[] args) {
-
-        Calculator c1 = new Calculator();
-//        c1.addNum(100);
-        c1.addNum(100).addNum(200).addNum(100);
-//        System.out.println(c1.result);
-//        c1 = c1.addNum(100);
-//        c1 = c1.addNum(200);
-//        c1 = c1.addNum(100);
-
-
-    }
-
+    // addNum is a method to add number to current result
+    // and return same Calculator object
+    // this will allow method chaining
+    // we can keep calling the method with same object
+    // obj.add(100).add(50).add(200)
+    // only one object . multiple method calls with chaining
+    // this is called builder pattern in Java
     public Calculator addNum(int num) {
 
         this.result += num;
+        // below code will create new object different than the current object
 //        Calculator c = new Calculator();
 //        return c;
+        // in order to just return current object after adding number
+        // we can use this -->> THE CURRENT OBJECT WE ARE WORKING ON !
         // it will return the same object you used to call this method
         // it will allow method chaining using same object
         return this;
---
+++ src/day55/CalculatorTest.java
@@ -0,0 +1,18 @@
+package day55;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+
+        Calculator c1 = new Calculator();
+//        c1.addNum(100);
+        c1.addNum(100).addNum(200).addNum(100);
+//        System.out.println(c1.result);
+//        c1 = c1.addNum(100);
+//        c1 = c1.addNum(200);
+//        c1 = c1.addNum(100);
+
+
+    }
+
+}
