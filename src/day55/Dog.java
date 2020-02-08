6553eca9ff1c4cbb6b3791b97f0369c04f2cf766
studio2recording <github@cybertekschool.com>
2020-02-08 13:12:17 -0500

    day 55 : Dog blue print for Dog Objects

---
+++ src/day55/Dog.java
@@ -0,0 +1,15 @@
+package day55;
+
+public class Dog {
+
+    String type;
+
+    public Dog(String type) {
+        this.type = type;
+    }
+
+    public void bark() {
+        System.out.println(type + " Dog is barking");
+    }
+
+}
