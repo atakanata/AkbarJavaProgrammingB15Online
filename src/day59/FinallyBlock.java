a10ded96bb8ced79353f5296f60b9712396d2c73
studio2recording <github@cybertekschool.com>
2020-02-15 15:54:12 -0500

    Day 59 : Finally block that always run

---
+++ src/day59/FinallyBlock.java
@@ -0,0 +1,36 @@
+package day59;
+
+public class FinallyBlock {
+
+
+    public static void main(String[] args) {
+
+
+        // in some cases we want to run certain code
+        // no matter exception happen or not
+        // as part of try catch
+
+        int[] nums = {1, 4, 6};
+
+        try {
+
+            System.out.println("nums[0] = " + nums[0]);
+
+        } catch (Exception e) {
+            // HOW DO I GET THE MESSAGE OUT OF THE EXCEPTION OBJECT
+            // Any exception object (can) have message field
+            // and it can be extracted by calling getMessage method
+            System.out.println("Exception caught \n " + e.getMessage());
+
+        } finally {
+            System.out.println("THIS CODE WILL ALWAYS RUN NO MATTER WE HAVE EXCEPTION OR NOT ");
+        }
+
+
+        System.out.println("THE END ");
+
+
+    }
+
+
+}
