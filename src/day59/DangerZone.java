7517b7187cc29f57abd72a74b336acb116bbe611
studio2recording <github@cybertekschool.com>
2020-02-15 17:31:27 -0500

    Day 59 : throws keyword just document the method might throw an exception

--- src/day59/DangerZone.java
+++ src/day59/DangerZone.java
@@ -1,10 +1,28 @@
 package day59;
 
+import java.io.IOException;
+
 public class DangerZone {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//}throws IOException {
 
+
+        try {
+            playWithFire();
+        } catch (IOException e) {
+            System.out.println("GO HERE IF YOU GET BURNED!!!");
+        }
+
+
+    }
+
+    // this is a method that declare ,it might throw a checked exception IOException
+    // whoever calls it , handle the consequences themselves
+    public static void playWithFire() throws IOException {
+
+        System.out.println("some consequences might happen ");
+        throw new IOException("NOW YOU ACTUALLY GET BURNED");
 
     }
 
