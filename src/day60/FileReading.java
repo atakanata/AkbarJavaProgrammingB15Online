84342c57fcabab4cc40996696fee83e4240c8e96
studio2recording <github@cybertekschool.com>
2020-02-16 13:37:37 -0500

    Day 60 : Reading the file using Files.readAllLines method

--- src/day60/FileReading.java
+++ src/day60/FileReading.java
@@ -10,17 +10,19 @@ public class FileReading {
 
     public static void main(String[] args) throws IOException {
 
-
         // reading a file is one line of code as below
         // it return List of String as each line as element
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get("src/day60/note.txt"));
+            System.out.println("allLines = " + allLines);
 
-        List<String> allLines = Files.readAllLines(Paths.get("src/day60/note.txt"));
-        System.out.println("allLines = " + allLines);
-
-        for (String eachLine : allLines) {
-            System.out.println(eachLine);
+            for (String eachLine : allLines) {
+                System.out.println(eachLine);
+            }
+        } catch (Exception e) {
+            System.out.println("BOOM!!");
+            System.out.println(e.getMessage());
         }
-
 
     }
 
