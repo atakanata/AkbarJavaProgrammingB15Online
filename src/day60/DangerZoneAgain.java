79f3e5bd9784374a262581948eba1d4a11cb7da1
studio2recording <github@cybertekschool.com>
2020-02-16 12:26:00 -0500

    Day 60 : Review Exception

---
+++ src/day60/DangerZoneAgain.java
@@ -0,0 +1,26 @@
+package day60;
+
+public class DangerZoneAgain {
+
+
+    public static void main(String[] args) {
+
+
+        readMyFile();
+
+
+    }
+    // document this method to tell the caller , the file you are about to read might not exists
+    // and handle the consequences yourself
+    // the exception type already exists for this type of situation is called
+    // FileNotFoundException from java.util package
+
+    public static void readMyFile() {
+
+        System.out.println("Reading the file in my computer");
+
+
+    }
+
+
+}
