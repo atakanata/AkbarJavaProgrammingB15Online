0ee0124095ffb05f644fa34a40687959c78f3580
studio2recording <github@cybertekschool.com>
2020-02-16 13:44:16 -0500

    Day 60 : Reading the file using Files.readAllLines method

--- src/day60/FileReading.java
+++ src/day60/FileReading.java
@@ -8,6 +8,17 @@ import java.util.List;
 public class FileReading {
 
 
+    // First create a file by right clicking day60 package and select new -- File
+    // name your file as note.txt
+    // enter few line of text
+    // close the file (it might ask you to save , if it did not it means it already saved)
+    // then use
+    // List<String> allLines = Files.readAllLines(Paths.get("src/day60/note.txt"));
+    // the method declare to throw checked exception so HANDLE OR DECLARE  !!
+    // for reading the file and storing it into the list
+    // make sure the path is correct , if not , it will throw exception
+    // print out the list content using loop to see whats inside line by line
+
     public static void main(String[] args) throws IOException {
 
         // reading a file is one line of code as below
