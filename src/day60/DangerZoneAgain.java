0ebd91e1cc44b0e20da7e7d4299faf44dbe5a23d
studio2recording <github@cybertekschool.com>
2020-02-16 12:35:59 -0500

    Day 60 : Review Exception | throw VS throws

--- src/day60/DangerZoneAgain.java
+++ src/day60/DangerZoneAgain.java
@@ -1,12 +1,23 @@
 package day60;
 
+import java.io.FileNotFoundException;
+
 public class DangerZoneAgain {
 
+    // Handle or Declare are the way to deal with checked exception
+    // if you are sure the method will never actually throw the exception
+    // it's good to just declare it to go through compiler to make it happy
+    // if there is actually a chance for the exception might left unhandled
+    // it's good to wrap up under try catch
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
 
-
+        //  calling a method that declare to throws checked exception
         readMyFile();
+        // Thread class is coming from java.lang package
+        // it has a method called sleep and it will let your program pause for x milliseconds
+        Thread.sleep(3000);
+        System.out.println("The end ");
 
 
     }
@@ -15,7 +26,7 @@ public class DangerZoneAgain {
     // the exception type already exists for this type of situation is called
     // FileNotFoundException from java.util package
 
-    public static void readMyFile() {
+    public static void readMyFile() throws FileNotFoundException {
 
         System.out.println("Reading the file in my computer");
 
