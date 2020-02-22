ac1aca9f99c5be0520fa87d379a937dd943959a5
studio2recording <github@cybertekschool.com>
2020-02-22 14:10:26 -0500

    Day 63 : Getting Letter Frequency using Map

---
+++ src/day63/FrequencyOfCharacters.java
@@ -0,0 +1,46 @@
+package day63;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequencyOfCharacters {
+
+
+    public static void main(String[] args) {
+
+        String str = "AAABBBBBBACCRDDD";
+
+
+        // Get the frequency and store into map   <Character, Integer>
+        // if we do not have the character
+        // we will enter character in char column
+        // and enter the count as 1
+
+        // if we have the character
+        // increase the existing count by 1
+        // and replace the value with new value
+        Map<Character, Integer> charFreq = new HashMap<>();
+
+        for (int x = 0; x < str.length(); x++) {
+
+            char currentChar = str.charAt(x);
+            // if we do not have the key yet , it means we are entering for the first time
+            // the count will be 1
+            if (!charFreq.containsKey(currentChar)) {
+                System.out.println("Enter for the first time " + currentChar);
+                charFreq.put(currentChar, 1);
+
+            } else {
+                // if we come to this point it means it has already showed up
+                // so we update the count with 1 extra than existing count
+                charFreq.replace(currentChar, charFreq.get(currentChar) + 1);
+
+            }
+        }
+        System.out.println("charFreq = " + charFreq);
+
+
+    }
+
+
+}
