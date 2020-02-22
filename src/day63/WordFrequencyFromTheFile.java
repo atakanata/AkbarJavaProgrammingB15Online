6f03285c83bb8dec0d0387c8ccdb8adec467088a
studio2recording <github@cybertekschool.com>
2020-02-22 16:14:01 -0500

    Day 63 : OPTIONALLY READING FILE and getting frequency

--- src/day63/WordFrequencyFromTheFile.java
+++ src/day63/WordFrequencyFromTheFile.java
@@ -1,9 +1,31 @@
 package day63;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
 public class WordFrequencyFromTheFile {
 
 
     public static void main(String[] args) {
+
+
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get("src/day63/map.txt"));
+            System.out.println("allLines = " + allLines);
+
+            String allLinesAsString = allLines.toString();
+
+
+            Map<String, Integer> map = WordUtil.getFrequencyMap(allLinesAsString);
+            System.out.println("map = " + map);
+
+
+        } catch (IOException e) {
+            System.out.println("ERROR@@@!!!!!");
+        }
 
 
     }
