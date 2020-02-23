f8729090d3963be23ea40a4d46bf40c200c10934
studio2recording <github@cybertekschool.com>
2020-02-23 13:42:11 -0500

    Day 64 : LinkedHashMap keep the insertion order compared to HashMap does not

--- src/day64/LinkedHashMapPractice.java
+++ src/day64/LinkedHashMapPractice.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class LinkedHashMapPractice {
 
     public static void main(String[] args) {
+        // Use LinkedHashMap if you want the insertion order
 
         Map<String, Double> groceryPriceMap = new LinkedHashMap<>();
         groceryPriceMap.put("Tomato", 1.99);
@@ -15,8 +16,10 @@ public class LinkedHashMapPractice {
         groceryPriceMap.put("Apple", 1.89);
 
         System.out.println("groceryPriceMap = " + groceryPriceMap);
-
-
+        // for LinkedHashMap
+//        {Tomato=1.99, Patato=2.99, Grape=3.99, Banana=1.99, Apple=1.89}
+        // for HashMap
+        // {Apple=1.89, Grape=3.99, Patato=2.99, Tomato=1.99, Banana=1.99}
     }
 
 
